8587c2b65c4a5488924142379ce2e0d9c042e1d9
mhlehri <lehrimirza101@gmail.com>
2023-12-19 23:33:47 +0600

    3rd button edited

--- src/Buttons/Buttons1/Button1.jsx
+++ src/Buttons/Buttons1/Button1.jsx
@@ -5,12 +5,16 @@ const Button1 = () => {
       <button className="bg-red-600 hover:scale-95  text-white duration-300">
         Hover
       </button>
-      <button className="hover:bg-black hover:text-white hover:scale-95 bg-transparent border-2 hover:border-transparent border-black duration-300">
+      <button className="hover:bg-white hover:text-black text-white hover:scale-95 bg-transparent border-2 hover:border-transparent border-white duration-300">
         Hover
       </button>
-      <button className="bg-teal-600 hover:text-white scale-95 relative overflow-hidden group">
-        <span className="absolute bg-red-500 w-2/3 ease-linear inset-0 -left-full group-hover:left-0 duration-300 -z-10"></span>
-        <span className="absolute bg-gray-500 ml-auto w-1/3 ease-linear inset-0 -right-full group-hover:right-0 duration-300 -z-10"></span>
+      <button className="bg-white text-black relative overflow-hidden group z-10 rounded-lg">
+        <span className="absolute bg-teal-200 rotate-12 -inset-44 group-hover:duration-500 duration-1000 scale-x-0 group-hover:scale-x-100 origin-left transform transition-transform"></span>
+        <span className="absolute bg-teal-500 rotate-12 -inset-44 group-hover:duration-1000 duration-700 scale-x-0 group-hover:scale-x-100 origin-left transform transition-transform"></span>
+        <span className="absolute bg-teal-600 rotate-12 -inset-44 group-hover:duration-700 duration-500 scale-x-0 group-hover:scale-x-50 origin-left transform transition-transform"></span>
+        <span className="absolute opacity-0 group-hover:opacity-100 duration-100 group-hover:duration-1000 ease-out text-center z-10">
+          Leave
+        </span>
         Hover
       </button>
     </>
