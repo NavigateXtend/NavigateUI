94be455421c1cb5f748d5703808eb886fc9c79b0
mhlehri <lehrimirza101@gmail.com>
2023-12-20 13:25:56 +0600

    nav links added

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -3,8 +3,15 @@ import { Link, Outlet } from "react-router-dom";
 const MainLayout = () => {
   return (
     <div>
-      <Link to="button" className="text-white">
-        button
+      <Link to="button">
+        <button className="text-3xl w-32 h-16 bg-red-600 hover:scale-95 text-white duration-300">
+          Button
+        </button>
+      </Link>
+      <Link to="slider">
+        <button className="text-3xl w-32 h-16 bg-red-600 hover:scale-95 text-white duration-300">
+          Slider
+        </button>
       </Link>
       <Outlet />
     </div>
