eaaa6f8d2480770e074dabb6654f7614046c242c
mhlehri <lehrimirza101@gmail.com>
2023-12-27 19:51:14 +0600

    form 2 added

--- src/Forms/Form.jsx
+++ src/Forms/Form.jsx
@@ -1,13 +1,11 @@
-import Form1 from "./Form1/Form1";
+import Form1 from "./Part1/Form1";
+import Form2 from "./Part1/Form2";
 
 const Form = () => {
   return (
-    <div>
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
       <Form1></Form1>
-      <Form1></Form1>
-      <Form1></Form1>
-      <Form1></Form1>
-      <Form1></Form1>
+      <Form2></Form2>
       <Form1></Form1>
     </div>
   );
---
+++ src/Forms/Part1/Form2.jsx
@@ -0,0 +1,42 @@
+const Form2 = () => {
+  return (
+    <div className="w-full md:w-1/2 mx-auto lg:w-[500px] drop-shadow-lg bg-white">
+      <form className="p-12">
+        <h1 className="backdrop-blur-sm text-4xl pb-8">Login</h1>
+        <div className="space-y-5">
+          <label htmlFor="email" className="block">
+            Email
+          </label>
+          <div className="relative">
+            <input
+              id="email"
+              type="email"
+              placeholder="example@gmail.com"
+              className="p-3 block w-full pl-10 drop-shadow-lg outline-none "
+            />
+            <span className="absolute top-1/4 left-2">👻</span>
+          </div>
+          <label htmlFor="password" className="block">
+            Password
+          </label>
+          <div className="relative">
+            <input
+              id="pass"
+              type="password"
+              placeholder=".............."
+              className="p-3 block w-full pl-10 drop-shadow-lg outline-none"
+            />
+            <span className="absolute top-1/4 left-2">🔒</span>
+          </div>
+        </div>
+
+        {/* button type will be submit for handling form submission*/}
+        <button type="button" className="py-2 px-5 mb-4 mt-6 shadow-lg ">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Form2;
