c447072949620ed168c9c9d8fa76046e5eef7585
shiyam-sarker10 <kaneki438500929@gmail.com>
2023-12-28 15:50:52 +0600

    added some changes

--- src/Cards/Card.jsx
+++ src/Cards/Card.jsx
@@ -4,7 +4,9 @@ import Card2 from "./Card2/Card2";
 const Card = () => {
   return (
     <div className="flex gap-6">
+      {/* Card 1  */}
       <Card1></Card1>
+      {/* Card 2  */}
       <Card2></Card2>
     </div>
   );
