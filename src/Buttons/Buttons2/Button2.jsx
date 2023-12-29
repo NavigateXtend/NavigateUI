557d7e18e895ab15f7f37196d1c6938f45ed76b4
mhlehri <lehrimirza101@gmail.com>
2023-12-30 02:13:10 +0600

    form 2 edited

---
+++ src/Buttons/Buttons2/Button2.jsx
@@ -0,0 +1,12 @@
+const Button2 = () => {
+  return (
+    <div className="con">
+      <a href="" className="button">
+        <span className="button-text">button</span>
+        <span className="button-icon">*</span>
+      </a>
+    </div>
+  );
+};
+
+export default Button2;
--- src/Forms/Part1/Form1.jsx
+++ src/Forms/Part1/Form1.jsx
@@ -1,7 +1,14 @@
 const Form1 = () => {
   return (
     <div className="w-full mx-auto lg:w-[800px] border-2 border-black drop-shadow-lg bg-white flex">
-      <div className="flex-1 w-full h-full bg-blue-500"></div>
+      <div className="flex-1 w-full h-full bg-blue-500 relative overflow-hidden hidden lg:block">
+        <h1 className="text-white text-2xl absolute bottom-3 right-3 text-right">
+          Hey! <br /> Welcome to
+          <br /> Unify
+        </h1>
+        <span className="bg-blue-800/30 w-32 h-32 -top-5 -left-5 rounded-full absolute z-20"></span>
+        <span className="bg-blue-800/20 w-36 h-36 -top-5 -left-5  rounded-full absolute z-10"></span>
+      </div>
       <form className="p-8 flex-1">
         <h1 className="backdrop-blur-sm text-4xl pb-4">Login</h1>
         <div className="space-y-5">
@@ -21,13 +28,14 @@ const Form1 = () => {
             id="password"
             type="password"
             placeholder=".............."
+            min={5}
             className="p-3 block w-full drop-shadow-lg outline-none border-2 invalid:border-red-700 valid:border-black"
           />
         </div>
 
         {/* button type will be submit for handling form submission*/}
         <button
-          type="submit"
+          type="button"
           className="py-2 px-5 mb-4 mt-8 shadow-lg border-2 border-black"
         >
           Submit
