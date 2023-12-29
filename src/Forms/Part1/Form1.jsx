fbb9c012a219c7e749221df47b2e1a3686624c8e
mhlehri <lehrimirza101@gmail.com>
2023-12-30 02:24:25 +0600

    form 1 edited

--- src/Forms/Part1/Form1.jsx
+++ src/Forms/Part1/Form1.jsx
@@ -1,12 +1,12 @@
 const Form1 = () => {
   return (
-    <div className="w-full mx-auto lg:w-[800px] border-2 border-black drop-shadow-lg bg-white flex">
+    <div className="w-full mx-auto lg:w-[800px] border-2 border-black drop-shadow-lg bg-white flex group">
       <div className="flex-1 w-full h-full bg-blue-500 relative overflow-hidden hidden lg:block">
         <h1 className="text-white text-2xl absolute bottom-3 right-3 text-right">
           Hey! <br /> Welcome to
           <br /> Unify
         </h1>
-        <span className="bg-blue-800/30 w-32 h-32 -top-5 -left-5 rounded-full absolute z-20"></span>
+        <span className="bg-blue-800/30 w-32 h-32 -top-8 -left-8 rounded-full absolute z-20 group-hover:w-56 group-hover:h-56 duration-500"></span>
         <span className="bg-blue-800/20 w-36 h-36 -top-5 -left-5  rounded-full absolute z-10"></span>
       </div>
       <form className="p-8 flex-1">
@@ -36,7 +36,7 @@ const Form1 = () => {
         {/* button type will be submit for handling form submission*/}
         <button
           type="button"
-          className="py-2 px-5 mb-4 mt-8 shadow-lg border-2 border-black"
+          className="py-2 px-5 mb-4 mt-8 overflow-hidden shadow-lg border-2 border-black before:block before:absolute before:translate-x-full before:inset-0 before:bg-black before:hover:translate-x-0  before:duration-300 before:rounded-s-full before:-z-10 after:-z-10 after:rounded-e-full after:duration-300 after:hover:translate-x-0 after:block after:absolute after:-translate-x-full after:inset-0 after:bg-black relative inline-block hover:text-white"
         >
           Submit
         </button>
