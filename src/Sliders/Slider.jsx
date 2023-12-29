c27fdafdb42edf034ce53e31e8e00c188d8d176b
mhlehri <lehrimirza101@gmail.com>
2023-12-29 23:53:17 +0600

    update made

--- src/Sliders/Slider.jsx
+++ src/Sliders/Slider.jsx
@@ -1,5 +1,5 @@
-import { Slider1 } from "./Slider1/Slider1";
 import { Slider2 } from "./Slider2/Slider2";
+import { Slider1 } from "./Slider1/Slider1";
 
 const Slider = () => {
   return (
--- src/Sliders/Slider1/Slider1.jsx
+++ src/Sliders/Slider1/Slider1.jsx
@@ -1,97 +1,109 @@
 import { useState } from "react";
 
-export const SliderMain = () => {
+export const Slider1 = () => {
   const [currentSlider, setCurrentSlider] = useState(0);
 
   const sliders = [
     {
-      img: "https://source.unsplash.com/600x600/?bedroom",
-      tags: "Room",
+      img: "https://source.unsplash.com/1200x1200/?beach",
     },
     {
-      img: "https://source.unsplash.com/600x600/?room",
-      tags: "Room",
+      img: "https://source.unsplash.com/1200x1200/?beach",
     },
     {
-      img: "https://source.unsplash.com/600x600/?livingrooms",
-      tags: "Room",
-    },
-    {
-      img: "https://source.unsplash.com/600x600/?livingroom",
-      tags: "Room",
-    },
-    {
-      img: "https://source.unsplash.com/600x600/?bedrooms",
-      tags: "Room",
+      img: "https://source.unsplash.com/1200x1200/?beach",
     },
   ];
 
+  const prevSlider = () => {
+    if (currentSlider === 0) {
+      return setCurrentSlider(sliders.length - 1);
+    }
+    return setCurrentSlider(currentSlider - 1);
+  };
   const nextSlider = () => {
     if (currentSlider === sliders.length - 1) {
       return setCurrentSlider(0);
     }
     return setCurrentSlider(currentSlider + 1);
   };
-
   return (
-    <div className="sm:w-2/3 relative overflow-hidden">
-      {/* arrow */}
-      <button
-        onClick={nextSlider}
-        className="absolute right-2 top-1/2 z-50 leading-0 text-2xl w-12 h-12 bg-white rounded-full text-[#b88e2f] shadow-md shadow-gray-400"
-      >
-        &gt;
-      </button>
-
-      {/* slider container */}
-      <div
-        className={`ease-linear duration-300 flex gap-[2%]`}
-        style={{ transform: `translateX(-${currentSlider * 52}%)` }}
-      >
-        {/* sliders */}
-        {sliders.map((slide, inx) => (
-          <div
-            key={inx}
-            className={`${
-              currentSlider === inx
-                ? "h-[310px] md:h-[480px] lg:h-[580px] "
-                : "h-[260px] md:h-[380px] lg:h-[480px]"
-            } min-w-[50%] relative duration-200`}
+    <div className="maxW-7xl mx-auto h-[540px] md:h-[670px] px-10 flex flex-col lg:flex-row items-center overflow-hidden gap-5 lg:gap-10 relative">
+      <div className="relative overflow-hidden">
+        {/* arrow */}
+        <button
+          onClick={prevSlider}
+          className="absolute left-2 top-1/2 z-50 bg-white rounded-full w-12 h-12 bgWhite "
+        >
+          <svg
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            transform="rotate(180)"
           >
-            <img src={slide.img} className="w-full h-full" alt="" />
-            <div
-              className={`absolute bottom-2 md:bottom-6 left-3 md:left-6 bg-[#FFFFFFB8] duration-700 transition p-4 ${
-                currentSlider === inx ? "opacity-1" : "opacity-0"
-              }`}
-            >
-              <h4 className="flex items-center md:gap-2 text-sm ">
-                {"0" + (inx + 1) + " - " + slide.tags}
-              </h4>
-              <h2 className="md:text-2xl font-semibold mt-1">Inner Peace</h2>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
-export const Slider1 = () => {
-  return (
-    <div className="max-w-7xl mx-auto h-[540px] md:h-[670px] px-10 flex flex-col lg:flex-row items-center overflow-hidden gap-5 lg:gap-10 relative">
-      <div className="bg-[#FCF8F3] w-full absolute left-0 h-[540px] md:h-[670px] -z-40"></div>
-      <div className="w-full lg:w-1/3 text-center lg:text-left space-y-2 lg:space-y-5 py-5">
-        <h1 className="text-2xl lg:text-[40px] font-bold">
-          50+ Beautiful rooms inspiration
-        </h1>
-        <p className="text-[#616161]">
-          Our designer already made a lot of beautiful prototipe of rooms that
-          inspire you
-        </p>
-        <button className="font-bold py-2 lg:py-3 hover:scale-95 duration-300 px-4 lg:px-10 text-white bg-[#B88E2F]">
-          Explore More
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              {" "}
+              <path
+                d="M13 15L16 12M16 12L13 9M16 12H8M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+                stroke="#b88e2f"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></path>{" "}
+            </g>
+          </svg>
         </button>
+        <button
+          onClick={nextSlider}
+          className="absolute right-2 top-1/2 bg-white rounded-full z-50 w-12 h-12 bgWhite "
+        >
+          <svg
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              {" "}
+              <path
+                d="M13 15L16 12M16 12L13 9M16 12H8M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+                stroke="#b88e2f"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></path>{" "}
+            </g>
+          </svg>
+        </button>
+
+        {/* slider container */}
+        <div
+          className={`ease-linear transform duration-300 flex`}
+          style={{ transform: `translateX(-${currentSlider * 100}%)` }}
+        >
+          {/* sliders */}
+          {sliders.map((slide, inx) => (
+            <div key={inx} className={`min-w-full relative duration-200`}>
+              <img
+                src={slide.img}
+                className="w-full h-full object-cover"
+                alt=""
+              />
+            </div>
+          ))}
+        </div>
       </div>
-      <SliderMain />
     </div>
   );
 };
