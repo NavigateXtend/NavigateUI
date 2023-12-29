d3062dd95a4d4befb90de2d3aa2d6f6e3a6f0fa1
mhlehri <lehrimirza101@gmail.com>
2023-12-29 10:45:22 +0600

    button 10 created

--- src/Buttons/Buttons1/Button1.jsx
+++ src/Buttons/Buttons1/Button1.jsx
@@ -62,8 +62,44 @@ const Button1 = () => {
         <span className="bg-indigo-400 group-hover:scale-125 scale-0 ease-in-out duration-300 delay-300 w-12 h-12 rounded-full absolute mx-auto my-auto inset-0 -z-10"></span>
       </button>
       {/* 9 */}
-      <button className="text-3xl w-32 h-16 before:absolute before:block before:inset-0 before:-z-10 before:bg-fuchsia-500 text-white after:block hover:after:w-full after:w-0  after:hover:left-0 after:right-0 after:top-0 after:h-full after:-z-10 after:duration-300 after:bg-fuchsia-950 after:absolute relative inline-block">
+      <button className="text-3xl w-32 h-16 before:absolute before:block before:inset-0 before:-z-10 before:bg-teal-500 text-white after:block hover:after:w-full after:w-0  after:hover:left-0 after:right-0 after:top-0 after:h-full after:-z-10 after:duration-300 after:bg-teal-900 after:absolute relative inline-block">
         Hover
+      </button>
+      {/* 10 */}
+      <button className="text-3xl box-border border-4 border-green-900 w-48 h-16 bg-green-600 text-white relative group">
+        <span className="pr-6">Download</span>
+        <span className="bg-green-900 absolute right-0 top-0  h-full flex items-center justify-center px-1 group-hover:duration-300 group-hover:w-full w-8 duration-300">
+          <svg
+            viewBox="0 0 24 24"
+            fill="none"
+            className="w-8 inline mx-auto "
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              {" "}
+              <path
+                d="M12 3V16M12 16L16 11.625M12 16L8 11.625"
+                stroke="#ffffff"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></path>{" "}
+              <path
+                d="M15 21H9C6.17157 21 4.75736 21 3.87868 20.1213C3 19.2426 3 17.8284 3 15M21 15C21 17.8284 21 19.2426 20.1213 20.1213C19.8215 20.4211 19.4594 20.6186 19 20.7487"
+                stroke="#ffffff"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></path>{" "}
+            </g>
+          </svg>
+        </span>
       </button>
     </>
   );
