f23c01ac10eacccd78249f8058d14686b5ca4fab
mhlehri <lehrimirza101@gmail.com>
2023-12-31 04:08:50 +0600

    dots added to slider 1

--- src/Sliders/Slider1/Slider1.jsx
+++ src/Sliders/Slider1/Slider1.jsx
@@ -27,7 +27,7 @@ export const Slider1 = () => {
     }
     return setCurrentSlider(currentSlider + 1);
   };
-
+  // if you don't want to change the slider automatically then you can just remove the useEffect
   useEffect(() => {
     const intervalId = setInterval(() => {
       nextSlider();
@@ -38,12 +38,12 @@ export const Slider1 = () => {
     };
   }, [currentSlider]);
   return (
-    <div className="maxW-7xl mx-auto h-[540px] md:h-[670px] px-10 flex flex-col lg:flex-row items-center overflow-hidden gap-5 lg:gap-10 relative">
+    <div className="max-w-7xl mx-auto h-[540px] md:h-[670px] flex flex-col lg:flex-row items-center overflow-hidden gap-5 lg:gap-10 px-10">
       <div className="relative overflow-hidden">
-        {/* arrow */}
+        {/* arrow left */}
         <button
           onClick={prevSlider}
-          className="absolute flex justify-center items-center left-2 top-1/2 z-50 bg-white rounded-full w-6 h-6 md:w-8 md:h-8 lg:w-12 lg:h-12 bgWhite "
+          className="absolute flex justify-center items-center left-2 top-1/2 z-50 bg-white rounded-full w-6 h-6 md:w-8 md:h-8 lg:w-12 lg:h-12"
         >
           <svg
             viewBox="0 0 1024 1024"
@@ -65,9 +65,10 @@ export const Slider1 = () => {
             </g>
           </svg>
         </button>
+        {/* arrow right */}
         <button
           onClick={nextSlider}
-          className="absolute flex justify-center items-center right-2 top-1/2 bg-white rounded-full z-50 w-12 h-12 bgWhite "
+          className="absolute flex justify-center items-center right-2 top-1/2 bg-white rounded-full z-50 w-12 h-12"
         >
           <svg
             viewBox="0 0 1024 1024"
@@ -90,21 +91,35 @@ export const Slider1 = () => {
             </g>
           </svg>
         </button>
+        {/* dots */}
+
+        <div className="flex justify-center items-center rounded-full z-50 absolute bottom-4 w-full">
+          {sliders.map((s, inx) => (
+            <button
+              key={inx}
+              onClick={() => {
+                setCurrentSlider(inx);
+              }}
+              className={`rounded-full duration-300 bg-sky-400 border-[3px] border-white ${
+                currentSlider === inx ? "w-10" : "w-3"
+              } h-3`}
+            ></button>
+          ))}
+        </div>
 
         {/* slider container */}
         <div
-          className={`ease-linear transform duration-300 flex`}
+          className={`ease-linear duration-300 flex transform-gpu relative`}
           style={{
             transform: `translateX(-${currentSlider * 100}%)`,
-            animation: ` 1s ease infinite`,
           }}
         >
           {/* sliders */}
           {sliders.map((slide, inx) => (
-            <div key={inx} className={`min-w-full relative duration-200`}>
+            <div key={inx} className={`min-w-full duration-200`}>
               <img
                 src={slide.img}
-                className="w-full h-full object-cover"
+                className="w-full h-[540px] md:h-[670px] object-cover"
                 alt={`Slider - ${inx + 1}`}
               />
             </div>
