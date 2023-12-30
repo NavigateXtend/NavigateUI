3e0f04f20f519d51e0b9a381ad3a9cf749cf3df4
mhlehri <lehrimirza101@gmail.com>
2023-12-30 21:45:01 +0600

    forms updated

--- src/Buttons/Buttons2/Button2.jsx
+++ src/Buttons/Buttons2/Button2.jsx
@@ -1,5 +1,10 @@
 const Button2 = () => {
-  return <div>{/* button 1 */}</div>;
+  return (
+    <div>
+      {/* button 1 */}
+      <button className="w-32 h-16">Button</button>
+    </div>
+  );
 };
 
 export default Button2;
