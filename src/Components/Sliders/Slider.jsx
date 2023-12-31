851b5d286c756de180f5527cce2c86522b2805db
mhlehri <lehrimirza101@gmail.com>
2023-12-31 20:12:58 +0600

    slider 3 added

--- src/Components/Sliders/Slider.jsx
+++ src/Components/Sliders/Slider.jsx
@@ -1,11 +1,13 @@
-import { Slider2 } from "./Slider2/Slider2";
 import { Slider1 } from "./Slider1/Slider1";
+import { Slider2 } from "./Slider2/Slider2";
+import { Slider3 } from "./Slider3/Slider3";
 
 const Slider = () => {
   return (
     <div>
       <Slider1></Slider1>
       <Slider2></Slider2>
+      <Slider3></Slider3>
     </div>
   );
 };
