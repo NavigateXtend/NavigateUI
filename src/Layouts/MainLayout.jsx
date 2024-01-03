780c02f444d89ce3b75f58e606446402fab706ef
shiyam-sarker10 <kaneki438500929@gmail.com>
2024-01-03 18:52:34 +0600

    fix mainlayout

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -62,7 +62,7 @@ const MainLayout = () => {
       </div>
       <div
         onClick={() => setIsOpen(false)}
-        className={`w-full  my-20 mx-auto ${
+        className={`w-full lg:w-[70%]  my-20 mx-auto ${
           isOpen ? "opacity-30 duration-500" : "duration-500 opacity-100"
         } lg:opacity-100`}
       >
