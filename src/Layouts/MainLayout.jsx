124b47bb9d3cde98f254399e518b18bbbae5992d
Mohammadarifcoding <minaraarifa@gmail.com>
2024-01-03 17:26:02 +0600

    Changed the layout

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -23,7 +23,7 @@ const MainLayout = () => {
           viewBox="0 0 24 24"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
-        >
+        >  
           <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
           <g
             id="SVGRepo_tracerCarrier"
