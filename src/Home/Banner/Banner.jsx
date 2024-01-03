e761bbe4f24704b57761a03294ac2516b3a5803c
Mohammadarifcoding <minaraarifa@gmail.com>
2024-01-03 23:55:00 +0600

    Made the code more easy to read

--- src/Home/Banner/Banner.jsx
+++ src/Home/Banner/Banner.jsx
@@ -3,27 +3,6 @@ import { Link } from "react-router-dom";
 const Banner = () => {
   return (
     <div className="">
-      {/* <div className="flex justify-center items-center flex-col">
-        <h2 className="text-[#1F2937] lg:text-[50px] md:text-[40px] sm:text-[30px] text-[25px] flex justify-center  font-extrabold  text-center max-w-[900px] mb-[20px] lg:mt-[120px] md:mt-[70px] mt-[50px]  ">
-          Free Task Management Software <br /> without any limits
-        </h2>
-        <p className=" md:text-lg sm:text-base text-sm max-w-[750px] px-5 text-center flex justify-center mb-[20px]">
-          Manage projects and tasks, collaborate with teammates, and status
-          reports at a glance with just one click
-        </p>
-      </div>
-
-      <div className="flex justify-center items-center">
-        <button className="bg-gradient-to-r flex items-center justify-center gap-2  hover:from-teal-700 hover:to-blue-600 from-teal-600 to-blue-500  font-semibold py-2 px-6 lg:text-xl text-lg mb-4 rounded-md shadow-md hover:shadow-lg">
-          Get Started . It's Free
-        </button>
-      </div>
-      <p className=" text-center md:text-base text-sm">
-        Unlimited Users . Unlimited Task . Free Forever
-      </p>
-      <div className="max-w-[1000px]  mt-10 mx-auto xl:px-0 px-4">
-        <img className="w-full h-full" src="/images/banner.png" alt="" />
-      </div> */}
 <div className="flex flex-col items-center justify-center text-center mt-14 mb-8 md:my-20 px-4 md:px-0">
   <h2 className="text-3xl md:text-4xl font-semibold leading-[52px] md:leading-[60px] max-w-[600px] text-gray-800">
     <span className="">
