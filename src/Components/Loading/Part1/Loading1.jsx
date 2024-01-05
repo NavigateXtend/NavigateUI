16c58ecab3d3bef3f4c05b0633c4c02312f4540a
mhlehri <lehrimirza101@gmail.com>
2024-01-05 11:32:01 +0600

    spinner 7 added

--- src/Components/Loading/Part1/Loading1.jsx
+++ src/Components/Loading/Part1/Loading1.jsx
@@ -13,6 +13,11 @@ const Loading1 = () => {
         <div className="w-5 h-5 animate-[ping_2s_linear_infinite] border rounded-full border-sky-600"></div>
         <div className="w-5 h-5 animate-[ping_2s_linear_3s_infinite] border rounded-full border-sky-600 absolute"></div>
       </div>
+      <div className="w-10 h-10 flex gap-2 items-center justify-center">
+        <div className="w-2 h-5 animate-[ping_1.4s_linear_infinite] bg-sky-600"></div>
+        <div className="w-2 h-5 animate-[ping_1.8s_linear_infinite] bg-sky-600"></div>
+        <div className="w-2 h-5 animate-[ping_2s_linear_infinite] bg-sky-600"></div>
+      </div>
     </div>
   );
 };
