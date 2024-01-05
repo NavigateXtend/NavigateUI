830bc0616f1306d18685083ba8c620d2185ba6ce
mhlehri <lehrimirza101@gmail.com>
2024-01-05 11:30:40 +0600

    spinner 2 added

--- src/Components/Loading/Part1/Loading1.jsx
+++ src/Components/Loading/Part1/Loading1.jsx
@@ -4,6 +4,7 @@ const Loading1 = () => {
   return (
     <div className="flex gap-3">
       <div className="w-10 h-10 animate-[spin_2s_linear_infinite] rounded-full border-4 border-dashed border-sky-600"></div>
+      <div className="w-10 h-10 animate-[spin_2s_linear_infinite] rounded-full border-8 border-dotted border-sky-600"></div>
     </div>
   );
 };
