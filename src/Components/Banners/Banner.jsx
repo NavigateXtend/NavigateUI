edf94829cf0b53e2f510dd49e8529c037334590d
Mohammadarifcoding <minaraarifa@gmail.com>
2024-01-07 20:08:27 +0600

    Deleted banner 1 and banner 2 for better view and update

--- src/Components/Banners/Banner.jsx
+++ src/Components/Banners/Banner.jsx
@@ -1,15 +1,12 @@
 import React from "react";
-import Banner1 from "./Banner1/Banner1";
-import Banner2 from "./Banner2/Banner2";
-import Banner3 from "./Banner3/Banner3";
+import Banner1 from './Banner1/Banner1';
+
 
 
 const Banner = () => {
   return (
     <div className="space-y-5">
       <Banner1></Banner1>
-      <Banner2></Banner2>
-      <Banner3></Banner3>
     </div>
   );
 };
