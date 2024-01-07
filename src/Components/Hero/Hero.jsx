b5607cdf7ea0c7a80bcd7c9df53dd91afa5b9a1b
Mohammadarifcoding <minaraarifa@gmail.com>
2024-01-07 21:11:29 +0600

    Added a new hero and also change the setup for banner to hero

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Hero1 from "./Hero1/Hero1";
+import Hero2 from "./Hero2/Hero2";
+
+
+
+
+const Hero = () => {
+  return (
+    <div className="space-y-5">
+      <Hero1></Hero1>
+      <Hero2></Hero2>
+    </div>
+  );
+};
+
+export default Hero;
