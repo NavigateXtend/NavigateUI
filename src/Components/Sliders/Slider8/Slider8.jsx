d57ff75f176896a1790984544a9940b77dd00d00
Mahmud Hassan Lehri <lehrimirza101@gmail.com>
2024-01-08 21:20:39 +0600

    sliders updated

--- src/Components/Sliders/Slider8/Slider8.jsx
+++ src/Components/Sliders/Slider8/Slider8.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import CodeBox from '../../../Shared/CodeBox/CodeBox';
 
 const codeStr = `
@@ -25,45 +25,23 @@ export const Slider8 = () => {
         }
     ];
 
-    const prevSlider = () => {
-        setCurrentSlider((currentSlider) => (currentSlider === 0 ? sliders.length - 1 : currentSlider - 1));
-    };
     const nextSlider = () => {
         setCurrentSlider((currentSlider) => (currentSlider === sliders.length - 1 ? 0 : currentSlider + 1));
     };
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            nextSlider();
+        }, 3000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, [currentSlider]);
 
     return (
         <CodeBox codeStr={codeStr}>
             <div className="max-w-full min-w-[300px] resizable-code-box  mx-auto h-[240px] md:h-[470px] lg:h-[700px] flex flex-col lg:flex-row items-center overflow-hidden gap-5 lg:gap-10 px-10">
-                <div className="relative overflow-hidden border-2 border-black">
-                    <div className="absolute w-fit h-fit rotate-90 bottom-1/2 flex gap-5 z-50 px-5">
-                        {/* arrow left */}
-                        <button onClick={prevSlider} className="flex justify-center items-center hover:bg-white/30 rounded-full w-6 h-6 md:w-8 md:h-8">
-                            <svg viewBox="0 0 1024 1024" className="w-4 h-4 md:w-6 md:h-6 icon" xmlns="http://www.w3.org/2000/svg" fill="#000000">
-                                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                                <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
-                                <g id="SVGRepo_iconCarrier">
-                                    <path
-                                        fill="#0095FF"
-                                        d="M685.248 104.704a64 64 0 010 90.496L368.448 512l316.8 316.8a64 64 0 01-90.496 90.496L232.704 557.248a64 64 0 010-90.496l362.048-362.048a64 64 0 0190.496 0z"
-                                    ></path>
-                                </g>
-                            </svg>
-                        </button>
-                        {/* arrow right */}
-                        <button onClick={nextSlider} className="flex justify-center items-center hover:bg-white/30 rounded-full w-6 h-6 md:w-8 md:h-8">
-                            <svg viewBox="0 0 1024 1024" className="w-4 h-4 md:w-6 md:h-6 icon" xmlns="http://www.w3.org/2000/svg" fill="#000000" transform="rotate(180)">
-                                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                                <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
-                                <g id="SVGRepo_iconCarrier">
-                                    <path
-                                        fill="#0095FF"
-                                        d="M685.248 104.704a64 64 0 010 90.496L368.448 512l316.8 316.8a64 64 0 01-90.496 90.496L232.704 557.248a64 64 0 010-90.496l362.048-362.048a64 64 0 0190.496 0z"
-                                    ></path>
-                                </g>
-                            </svg>
-                        </button>
-                    </div>
+                <div className="relative overflow-hidden">
                     {/* dots */}
                     <div className="flex h-fit rounded-full z-50 absolute right-0 bottom-1/2 w-fit gap-1 rotate-90">
                         {sliders.map((_, inx) => (
