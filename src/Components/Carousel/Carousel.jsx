ab8b4f687ed362c73372aaee2f33b2245366f2f7
Mohammadarifcoding <minaraarifa@gmail.com>
2024-01-13 21:36:10 +0600

    Added the scroll effect form the top feature

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { Carousel1 } from './Carousel1/Carousel1';
 import { Carousel2 } from './Carousel2/Carousel2';
 import { Carousel3 } from './Carousel3/Carousel3';
@@ -8,6 +9,14 @@ import { Carousel7 } from './Carousel7/Carousel7';
 import { Carousel8 } from './Carousel8/Carousel8';
 
 const Slider = () => {
+    // For starting the scroll form the top
+    useEffect(() => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }, []);
+
     return (
         <div className="flex flex-col justify-center items-center">
             <Carousel1></Carousel1>
