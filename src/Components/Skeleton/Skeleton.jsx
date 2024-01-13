c9e00860d109e37003d9e64b13ffc841ad037396
Mohammadarifcoding <minaraarifa@gmail.com>
2024-01-13 16:06:03 +0600

    Added the skeleton loading 3 number

--- src/Components/Skeleton/Skeleton.jsx
+++ src/Components/Skeleton/Skeleton.jsx
@@ -1,16 +1,21 @@
 import Skeleton1 from "./Skeleton1/Skeleton1";
 import Skeleton2 from "./Skeleton2/Skeleton2";
+import Skeleton3 from "./Skeleton3/Skeleton3";
 
 const Skeleton = () => {
     return (
         <div className="grid grid-cols-1 space-y-10">
              <>
-              <h1 className="text-xl italic font-bold opacity-70 underline m-0 p-0">Simple Card Skeleton</h1>
+              <h1 className="text-xl italic font-bold opacity-70 underline m-0 p-0">Avatar loading skeleton</h1>
               <Skeleton1></Skeleton1>
           </>
           <>
               <h1 className="text-xl italic font-bold opacity-70 underline m-0 p-0">Ecommerce Card Skeleton</h1>
               <Skeleton2></Skeleton2>
+          </>
+          <>
+              <h1 className="text-xl italic font-bold opacity-70 underline m-0 p-0">Ecommerce Card Skeleton</h1>
+              <Skeleton3></Skeleton3>
           </>
         </div>
     );
