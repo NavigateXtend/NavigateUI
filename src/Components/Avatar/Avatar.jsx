cf4d14b72df90ce96f59fe57a06a44e7a25d10ec
shiyam-sarker10 <kaneki438500929@gmail.com>
2024-01-16 03:32:27 +0600

    added avatarType 4

--- src/Components/Avatar/Avatar.jsx
+++ src/Components/Avatar/Avatar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import AvatarType1 from './AvatarType1/AvatarType1';
 import AvatarType2 from './AvatarType2/AvatarType2';
 import AvatarType3 from './AvaterType3/AvatarType3';
+import AvatarType4 from './AvatarType4/AvatarType4';
 
 const Avatar = () => {
     return (
@@ -18,8 +19,13 @@ const Avatar = () => {
             </div>
             {/* Active avatar  */}
             <div>
-                <h1 className="text-2xl font-semibold px-4 mt-10 text-slate-800"># Active</h1>
+                <h1 className="text-2xl font-semibold px-4 mt-10 text-slate-800"># Active avatar</h1>
                 <AvatarType3></AvatarType3>
+            </div>
+            {/* avatar with Edit Icon  */}
+            <div>
+                <h1 className="text-2xl font-semibold px-4 mt-10 text-slate-800"># Avatar with edit icon</h1>
+                <AvatarType4/>
             </div>
         </div>
     );
