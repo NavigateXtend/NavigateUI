d44c488d698559804e1589719afd104896011c04
shiyam-sarker10 <kaneki438500929@gmail.com>
2024-01-16 16:50:09 +0600

    added testimonial slider & accordion with image slide

--- src/Components/Accordion/Accordion11/Accordion11.jsx
+++ src/Components/Accordion/Accordion11/Accordion11.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import CodeBox from '../../../Shared/CodeBox/CodeBox';
 const codeStr = `import { useState } from 'react';
 
-export const Carousel6 = () => {
+export const Accordion11 = () => {
   const [isOpen, setIsOpen] = useState(null);
   const toggle = ({ currentIdx }) => setIsOpen((prevIdx) => (prevIdx == currentIdx ? null : currentIdx));
   const sliders = [{img: "https://source.unsplash.com/1200x640/?snow-fall",title: "Winter",des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",},{img: "https://source.unsplash.com/1200x640/?Spring",title: "Spring",des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",},{img: "https://source.unsplash.com/1200x640/?sea-beach",title: "Summer",des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",},{img: "https://source.unsplash.com/1200x640/?Autumn",title: "Autumn",des: "A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.",},];
@@ -24,7 +24,7 @@ export const Carousel6 = () => {
       </div>
   )};`;
 
-export const Carousel6 = () => {
+export const Accordion11 = () => {
     const [isOpen, setIsOpen] = useState(null);
 
     const toggle = ({ currentIdx }) => setIsOpen((prevIdx) => (prevIdx == currentIdx ? null : currentIdx));
@@ -67,16 +67,16 @@ export const Carousel6 = () => {
                         <img className="h-full object-cover rounded-3xl" src={slide.img} alt="" />
 
                         <img
-                            className={`absolute bottom-5 border-white border md:border-2 ${
+                            className={`absolute bottom-5 transition-all duration-500 border-white border md:border-2 ${
                                 isOpen === idx ? 'left-4' : 'left-1/2 -translate-x-1/2 duration-700'
-                            } h-[30px] md:h-[50px] w-[30] md:w-[50px] object-cover rounded-full`}
+                            } h-10 w-10 md:h-[50px] md:w-[50px] object-cover rounded-full`}
                             src={slide.img}
                             alt=""
                         />
 
-                        <div className={`text-white absolute left-[100px] bottom-5`}>
-                            <h3 className="text-3xl font-semibold">Status</h3>
-                            <p className="text-xl">{slide.title}</p>
+                        <div className={`text-white absolute left-[45%] md:left-[100px] bottom-5 transition-all duration-300 ${isOpen === idx ? "opacity-100" : "opacity-0 "}`}>
+                            <h3 className="text-lg md:text-3xl font-semibold">Status</h3>
+                            <p className="text-sm md:text-xl">{slide.title}</p>
                         </div>
                     </div>
                 ))}
