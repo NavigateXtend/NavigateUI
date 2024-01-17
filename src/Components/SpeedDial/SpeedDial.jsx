db6f68f16c7b8c756fff872a52d73ed00e0be9b3
shiyam-sarker10 <kaneki438500929@gmail.com>
2024-01-18 01:26:59 +0600

    Added : Speed Dial Default

--- src/Components/SpeedDial/SpeedDial.jsx
+++ src/Components/SpeedDial/SpeedDial.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { SpeedDial1 } from './SpeedDial1/SpeedDial1';
 
 const SpeedDial = () => {
+    // For starting the scroll form the top
+    useEffect(() => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }, []);
     return (
         <div>
             <h1>Default Speed Dial</h1>
+            <SpeedDial1></SpeedDial1>
         </div>
     );
 };
