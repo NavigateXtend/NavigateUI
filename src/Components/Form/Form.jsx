828909c8cb4bd8575786473d8c021895e4eb2f5b
Mohammadarifcoding <minaraarifa@gmail.com>
2024-01-22 09:15:23 +0600

    Update names in the form and card section

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -18,19 +18,19 @@ const Form = () => {
 
     return (
         <div className="grid grid-cols-1 space-y-10">
-            <h1 className="text-xl">Login Form With Social Icons</h1>
+            <h1 className="text-2xl font-semibold px-4 mt-10 text-slate-800"># Login Form With Social Icons</h1>
             <Form1></Form1>
-            {/* <h1 className="text-xl">@Simple Login Form</h1>
+            {/* <h1 className="text-2xl font-semibold px-4 mt-10 text-slate-800"># Simple Login Form</h1>
             <Form2></Form2>
-            {/* <h1 className="text-xl">#Form</h1>
+            {/* <h1 className="text-2xl font-semibold px-4 mt-10 text-slate-800">#Form</h1>
             <Form3></Form3> */}
-            <h1 className="text-xl">@Simple Form</h1>
+            <h1 className="text-2xl font-semibold px-4 mt-10 text-slate-800"># Simple Form</h1>
             <Form4></Form4>
-            <h1 className="text-xl">@Simple Login Form</h1>
+            <h1 className="text-2xl font-semibold px-4 mt-10 text-slate-800"># Simple Login Form</h1>
             <Form5></Form5>
-            <h1 className="text-xl">@Simple Login Form</h1>
+            <h1 className="text-2xl font-semibold px-4 mt-10 text-slate-800"># Simple Login Form</h1>
             <Form6></Form6>
-            <h1 className="text-xl">@Switchable Login & Register Form</h1>
+            <h1 className="text-2xl font-semibold px-4 mt-10 text-slate-800"># Switchable Login & Register Form</h1>
             <Form7></Form7>
         </div>
     );
