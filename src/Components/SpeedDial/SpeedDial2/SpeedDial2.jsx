5b2df83a85355d7f59a24c98bdae63e3f6c0e3cb
Mohammadarifcoding <minaraarifa@gmail.com>
2024-01-23 10:08:18 +0600

    Added tooltip 1 with codebox setup

--- src/Components/SpeedDial/SpeedDial2/SpeedDial2.jsx
+++ src/Components/SpeedDial/SpeedDial2/SpeedDial2.jsx
@@ -1,11 +1,5 @@
-import React from 'react';
-
 const SpeedDial2 = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    return <div></div>;
 };
 
 export default SpeedDial2;
---
+++ src/Components/Tooltip/Tooltip.jsx
@@ -0,0 +1,12 @@
+import Tooltip1 from "./Tooltip1/Tooltip1";
+
+const Tooltip = () => {
+    return (
+        <div>
+            <h2 className="md:text-2xl text-xl font-semibold px-4 mt-10 text-slate-800"># Simple text tooltip</h2>
+            <Tooltip1></Tooltip1>
+        </div>
+    );
+};
+
+export default Tooltip;
