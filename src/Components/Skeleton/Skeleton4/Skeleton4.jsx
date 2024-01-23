85a063a1470699d1b9586f0e2030053e539696f0
Mohammadarifcoding <minaraarifa@gmail.com>
2024-01-23 09:33:23 +0600

    Update skeleton 4 and added dark theme

--- src/Components/Skeleton/Skeleton4/Skeleton4.jsx
+++ src/Components/Skeleton/Skeleton4/Skeleton4.jsx
@@ -2,35 +2,65 @@ import { useState } from 'react';
 import CodeBox from '../../../Shared/CodeBox/CodeBox';
 
 const Skeleton4 = () => {
-    const [isModalOpen, setModalOpen] = useState(false);
-
-    const openModal = () => setModalOpen(true);
-    const closeModal = () => setModalOpen(false);
     const codeStr = `
-<div className=" p-6 rounded-md shadow-md">
-    <div className="animate-pulse">
-        {/* Product Image Skeleton */}
-        <div className="w-64 lg:h-64 md:h-52 h-48 rounded-lg bg-gray-300 mb-4"></div>
-        {/* Product Title Skeleton */}
-        <div className="w-2/3 h-8 rounded-lg bg-gray-300 mb-4"></div>
-        {/* Product Description Skeleton */}
-        <div className="w-full h-16 rounded-lg bg-gray-300 mb-4"></div>
-    </div>
-</div>
+                {/* Light theme skeleton */}
+                <div className=" p-6 rounded-md shadow-md mx-auto max-w-fit">
+                    <div className="animate-pulse">
+                        {/* Product Image Skeleton */}
+                        <div className="w-[300px] lg:h-52 md:h-52 h-48 rounded-lg bg-gray-300 mb-6"></div>
+                        {/* Product Title Skeleton */}
+                        <div className="w-[290px] h-4 rounded-lg bg-gray-300 mb-4"></div>
+                        {/* product heading skeleton */}
+                        <div className="w-[220px] h-4 rounded-lg bg-gray-300 mb-4"></div>
+                        {/* Product Description Skeleton */}
+                        <div className="w-[200px] h-4 rounded-lg bg-gray-300 mb-4"></div>
+                    </div>
+                </div>
+                {/* Dark theme skeleton */}
+
+                <div className="p-6 rounded-md shadow-md mx-auto max-w-fit bg-[#657287] ">
+                    <div className="animate-pulse">
+                        {/* Product Image Skeleton */}
+                        <div className="w-[300px] lg:h-52 md:h-52 h-48 rounded-lg bg-[#9FADC2] mb-6"></div>
+                        {/* Product Title Skeleton */}
+                        <div className="w-[290px] h-4 rounded-lg bg-[#9FADC2] mb-4"></div>
+                        {/* Product Heading Skeleton */}
+                        <div className="w-[220px] h-4 rounded-lg bg-[#9FADC2] mb-4"></div>
+                        {/* Product Description Skeleton */}
+                        <div className="w-[200px] h-4 rounded-lg bg-[#9FADC2] mb-4"></div>
+                    </div>
+                </div>
   `;
 
     return (
         <CodeBox codeStr={codeStr}>
-            <div className=" p-6 rounded-md shadow-md">
-                <div className="animate-pulse">
-                    {/* Product Image Skeleton */}
-                    <div className="w-[300px] lg:h-52 md:h-52 h-48 rounded-lg bg-gray-300 mb-6"></div>
-                    {/* Product Title Skeleton */}
-                    <div className="w-[290px] h-4 rounded-lg bg-gray-300 mb-4"></div>
-                    {/* product heading skeleton */}
-                    <div className="w-[220px] h-4 rounded-lg bg-gray-300 mb-4"></div>
-                    {/* Product Description Skeleton */}
-                    <div className="w-[200px] h-4 rounded-lg bg-gray-300 mb-4"></div>
+            <div className="flex gap-4 justify-center lg:flex-row flex-col">
+                {/* Light theme skeleton */}
+                <div className=" p-6 rounded-md shadow-md mx-auto max-w-fit">
+                    <div className="animate-pulse">
+                        {/* Product Image Skeleton */}
+                        <div className="w-[300px] lg:h-52 md:h-52 h-48 rounded-lg bg-gray-300 mb-6"></div>
+                        {/* Product Title Skeleton */}
+                        <div className="w-[290px] h-4 rounded-lg bg-gray-300 mb-4"></div>
+                        {/* product heading skeleton */}
+                        <div className="w-[220px] h-4 rounded-lg bg-gray-300 mb-4"></div>
+                        {/* Product Description Skeleton */}
+                        <div className="w-[200px] h-4 rounded-lg bg-gray-300 mb-4"></div>
+                    </div>
+                </div>
+                {/* Dark theme skeleton */}
+
+                <div className="p-6 rounded-md shadow-md mx-auto max-w-fit bg-[#657287] ">
+                    <div className="animate-pulse">
+                        {/* Product Image Skeleton */}
+                        <div className="w-[300px] lg:h-52 md:h-52 h-48 rounded-lg bg-[#9FADC2] mb-6"></div>
+                        {/* Product Title Skeleton */}
+                        <div className="w-[290px] h-4 rounded-lg bg-[#9FADC2] mb-4"></div>
+                        {/* Product Heading Skeleton */}
+                        <div className="w-[220px] h-4 rounded-lg bg-[#9FADC2] mb-4"></div>
+                        {/* Product Description Skeleton */}
+                        <div className="w-[200px] h-4 rounded-lg bg-[#9FADC2] mb-4"></div>
+                    </div>
                 </div>
             </div>
         </CodeBox>
