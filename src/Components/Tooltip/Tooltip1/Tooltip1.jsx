595984b90a6caba149efad5e3a23c73bc34b6cc5
Mohammadarifcoding <minaraarifa@gmail.com>
2024-01-27 22:33:42 +0600

    Updated two from and also solved the tooltip hover issue

--- src/Components/Tooltip/Tooltip1/Tooltip1.jsx
+++ src/Components/Tooltip/Tooltip1/Tooltip1.jsx
@@ -1,24 +1,22 @@
 import CodeBox from '../../../Shared/CodeBox/CodeBox';
 
 const Tooltip1 = () => {
-  
     const codestr = `
-<div className="flex justify-center group relative my-10">
+<div className="flex justify-center items-center group h-10 relative my-10">
     {/* Hover button */}
     <button className="px-3 py-2 bg-slate-100 border border-slate-700 rounded-md">Hover on me</button>
     {/* Hover Text */}
-    <p className="absolute -top-12   duration-700 opacity-0 group-hover:opacity-100 scale-0 group-hover:scale-100 bg-blue-700 px-2 py-1 text-white rounded-md">This is the hover text</p>
+    <p className="absolute -top-12 duration-600 hidden opacity-0 group-hover:opacity-100 group-hover:block bg-blue-700 px-2 py-1 text-white rounded-md">This is the hover text</p>
 </div>
-    `
-
+    `;
 
     return (
         <CodeBox codeStr={codestr}>
-            <div className="flex justify-center group relative my-10">
+            <div className="flex justify-center items-center group h-10 relative my-10">
                 {/* Hover button */}
                 <button className="px-3 py-2 bg-slate-100 border border-slate-700 rounded-md">Hover on me</button>
                 {/* Hover Text */}
-                <p className="absolute -top-12   duration-700 opacity-0 group-hover:opacity-100 scale-0 group-hover:scale-100 bg-blue-700 px-2 py-1 text-white rounded-md">This is the hover text</p>
+                <p className="absolute -top-12 duration-600 hidden opacity-0 group-hover:opacity-100 group-hover:block bg-blue-700 px-2 py-1 text-white rounded-md">This is the hover text</p>
             </div>
         </CodeBox>
     );
