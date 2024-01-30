72ee713f048af8f689e94d3deb6859e57e61b6f0
shiyam-sarker10 <kaneki438500929@gmail.com>
2024-01-30 19:57:00 +0600

    working on Speed Dial

--- src/Components/SpeedDial/SpeedDial.jsx
+++ src/Components/SpeedDial/SpeedDial.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { SpeedDial1 } from './SpeedDial1/SpeedDial1';
+import SpeedDial2 from './SpeedDial2/SpeedDial2';
 
 const SpeedDial = () => {
     // For starting the scroll form the top
@@ -11,8 +12,15 @@ const SpeedDial = () => {
     }, []);
     return (
         <div>
-            <h1>Default Speed Dial</h1>
-            <SpeedDial1></SpeedDial1>
+            <div>
+                <h1 className="text-2xl font-semibold px-4 mt-10 text-slate-800"># Horizontal To Bottom Speed dial</h1>
+                <SpeedDial1></SpeedDial1>
+            </div>
+            <div>
+                <h1 className="text-2xl font-semibold px-4 mt-10 text-slate-800"># Horizontal To Bottom Speed dial</h1>
+                <SpeedDial2></SpeedDial2>
+            </div>
+            
         </div>
     );
 };
