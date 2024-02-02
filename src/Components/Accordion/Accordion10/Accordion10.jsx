1d693b974375a4d5235bde2f5b2322609e4a3fd6
shiyam-sarker10 <kaneki438500929@gmail.com>
2024-02-03 03:23:38 +0600

    _added : Accordion12, _modified: Accordion Class Consistency

--- src/Components/Accordion/Accordion10/Accordion10.jsx
+++ src/Components/Accordion/Accordion10/Accordion10.jsx
@@ -11,18 +11,14 @@ const Accordion10 = () => {
         setIsActive((prevIdx) => (prevIdx === idx ? null : idx));
     };
     return (
-            <div className="w-full min-h-[200px] md:min-h-[300px] flex bg-sky-400 mx-auto">
+            <div className="mx-auto flex min-h-[200px] w-full bg-sky-400 md:min-h-[300px]">
                 {accordionData.map((_, idx) => (
                     <div key={idx} className="flex">
                         {/* toggle item */}
-                        <button
-                            onClick={() => handleToggle(idx)}
-                            className={\`h-full flex justify-center items-center w-6 lg:w-14 bg-sky-500 \${isActive === idx && 'bg-lime-600'} text-white shadow-[2px_2px_5px_#00000083] z-20 relative\`}>
-                            <p>{idx + 1}</p>
-                        </button>
+                        <button onClick={() => handleToggle(idx)} className={\`flex h-full w-6 items-center justify-center bg-sky-500 lg:w-14 \${isActive === idx && 'bg-lime-600'} relative z-20 text-white shadow-[2px_2px_5px_#00000083]\`}><p>{idx + 1}</p></button>
                         {/* container */}
-                        <div className={\`\${isActive === idx ? 'md:w-56 px-2 md:px-5 opacity-1' : 'w-0 opacity-0'} grid place-content-center bg-white shadow-md z-10 text-black duration-300 ease-in-out\`}>
-                            <h2 className="lg:text-2xl font-black">{_.title}</h2>
+                        <div className={\`\${isActive === idx ? 'opacity-1 px-2 md:w-56 md:px-5' : 'w-0 opacity-0'} z-10 grid place-content-center bg-white text-black shadow-md duration-300 ease-in-out\`}>
+                            <h2 className="font-black lg:text-2xl">{_.title}</h2>
                             <p className="text-black/60">{_.subtitle}</p>
                         </div>
                     </div>
@@ -64,21 +60,21 @@ const Accordion10 = () => {
     };
     return (
         <CodeBox codeStr={codeStr}>
-            <div className="w-full min-h-[200px] md:min-h-[300px] flex bg-sky-400 mx-auto">
+            <div className="mx-auto flex min-h-[200px] w-full bg-sky-400 md:min-h-[300px]">
                 {accordionData.map((_, idx) => (
                     <div key={idx} className="flex">
                         {/* toggle item */}
                         <button
                             onClick={() => handleToggle(idx)}
-                            className={`h-full flex justify-center items-center w-6 lg:w-14 bg-sky-500 ${isActive === idx && 'bg-lime-600'} text-white shadow-[2px_2px_5px_#00000083] z-20 relative`}
+                            className={`flex h-full w-6 items-center justify-center bg-sky-500 lg:w-14 ${isActive === idx && 'bg-lime-600'} relative z-20 text-white shadow-[2px_2px_5px_#00000083]`}
                         >
                             <p>{idx + 1}</p>
                         </button>
                         {/* container */}
                         <div
-                            className={`${isActive === idx ? 'md:w-56 px-2 md:px-5 opacity-1' : 'w-0 opacity-0'} grid place-content-center bg-white shadow-md z-10 text-black duration-300 ease-in-out`}
+                            className={`${isActive === idx ? 'opacity-1 px-2 md:w-56 md:px-5' : 'w-0 opacity-0'} z-10 grid place-content-center bg-white text-black shadow-md duration-300 ease-in-out`}
                         >
-                            <h2 className="lg:text-2xl font-black">{_.title}</h2>
+                            <h2 className="font-black lg:text-2xl">{_.title}</h2>
                             <p className="text-black/60">{_.subtitle}</p>
                         </div>
                     </div>
