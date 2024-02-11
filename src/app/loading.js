2a35f3b35fba0648888a272f3950049d860cc5a2
Mohammadarifcoding <minaraarifa@gmail.com>
2024-02-11 20:06:10 +0600

    _updated: Loading and according 1

--- src/app/loading.js
+++ src/app/loading.js
@@ -2,9 +2,13 @@ import Image from 'next/image';
 
 export default function loading() {
     return (
-        <div className="item-center relative flex h-96 w-full justify-center">
+        <div className='flex justify-center item-center h-full w-full pt-[70px]'>
+  <div className=" relative flex h-96 w-full justify-center item-center">
             <span className="absolute -top-3 h-16 w-16 animate-[spin_1s_linear_infinite] rounded-full border-4 border-sky-400 border-l-transparent border-r-transparent"></span>
             <Image className="absolute" src="/Logo.svg" alt="navigateui logo" width={45} height={45} />
         </div>
+        </div>
+
+      
     );
 }
