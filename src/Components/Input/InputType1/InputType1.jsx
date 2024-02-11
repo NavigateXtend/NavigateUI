2d9047a4ace15730fc7ded3519843418f0e8a12d
shiyam-sarker10 <kaneki438500929@gmail.com>
2024-02-11 14:02:00 +0600

    refetch the updates

--- src/Components/Input/InputType1/InputType1.jsx
+++ src/Components/Input/InputType1/InputType1.jsx
@@ -12,7 +12,7 @@ export const InputType1 = () => {
     return (
         <CodeBox codeStr={codeStr}>
             <div className="">
-                <input className="rounded-lg border border-[#1B8EF8] px-4 py-2 text-[#1B8EF8] focus:outline-none" type="text" />
+                <input className="rounded-lg border bg-transparent border-[#1B8EF8] px-4 py-2 text-[#1B8EF8] focus:outline-none" type="text" />
             </div>
         </CodeBox>
     );
