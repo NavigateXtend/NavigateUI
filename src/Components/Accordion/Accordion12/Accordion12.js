0874144ab50c9607f80b59dd7f0905572a120536
Mahmud Hassan Lehri <lehrimirza101@gmail.com>
2024-02-12 11:56:42 +0600

    _updated : loading.js and accordion 12

---
+++ src/Components/Accordion/Accordion12/Accordion12.js
@@ -0,0 +1,61 @@
+'use client';
+import { useState } from 'react';
+import CodeBox from '@/Shared/CodeBox/CodeBox';
+
+const codeStr = `const Accordion12 = () => {
+    const sliders = [
+        { img: 'https://source.unsplash.com/1200x640/?snow-fall', title: 'Winter', des: 'A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.' },
+        { img: 'https://source.unsplash.com/1200x640/?Spring', title: 'Spring', des: 'A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.' },
+        { img: 'https://source.unsplash.com/1200x640/?sea-beach', title: 'Summer', des: 'A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.' },
+        { img: 'https://source.unsplash.com/1200x640/?Autumn', title: 'Autumn', des: 'A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.' }
+    ];
+    const [isOpen, setIsOpen] = useState(1);
+    const handleToggle = (idx) => setIsOpen((prevIdx) => (prevIdx == idx ? null : idx));
+    return (
+        <div className="flex h-[800px] items-center justify-center gap-4 bg-blue-50">
+            {sliders?.map((slide, idx) => (
+                <div onClick={() => handleToggle(idx)} key={idx} className={\`\${isOpen == idx ? 'w-[300px] opacity-100 shadow-lg' : 'w-[100px] opacity-70 grayscale'} \${idx % 2 == 0 ? 'translate-y-16' : ''} relative h-[600px] duration-500 ease-in-out\`}>
+                    <img className="block h-full w-full rounded-md object-cover shadow-md" src={slide?.img} alt="" />
+                    <div className={\`\${isOpen == idx ? 'opacity-100' : 'opacity-0'} absolute bottom-0 h-1/2 w-full bg-gradient-to-t from-black to-transparent duration-500 ease-in-out\`}>
+                        <h1 className={\`relative text-center text-2xl text-white/80 \${isOpen == idx ? 'top-[150px] opacity-100' : 'top-[200px] opacity-0'} duration-500 ease-in-out\`}>{slide?.title}</h1>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
+export default Accordion12;
+`;
+
+const Accordion12 = () => {
+    const sliders = [
+        { img: 'https://source.unsplash.com/1200x640/?snow-fall', title: 'Winter', des: 'A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.' },
+        { img: 'https://source.unsplash.com/1200x640/?Spring', title: 'Spring', des: 'A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.' },
+        { img: 'https://source.unsplash.com/1200x640/?sea-beach', title: 'Summer', des: 'A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.' },
+        { img: 'https://source.unsplash.com/1200x640/?Autumn', title: 'Autumn', des: 'A Symphony of Tranquility. Experience the perfect blend of relaxation and excitement.' }
+    ];
+    const [isOpen, setIsOpen] = useState(1);
+    const handleToggle = (idx) => setIsOpen((prevIdx) => (prevIdx == idx ? null : idx));
+    return (
+        <CodeBox codeStr={codeStr}>
+            <div className="flex h-[800px] items-center justify-center gap-4">
+                {sliders?.map((slide, idx) => (
+                    <div
+                        onClick={() => handleToggle(idx)}
+                        key={idx}
+                        className={`${isOpen == idx ? 'w-[300px] opacity-100 shadow-lg' : 'w-[100px] opacity-70 grayscale'} ${idx % 2 == 0 ? 'translate-y-16' : ''} relative h-[600px] duration-500 ease-in-out`}
+                    >
+                        <img className="block h-full w-full rounded-md object-cover shadow-md" src={slide?.img} alt="" />
+                        <div className={`${isOpen == idx ? 'opacity-100' : 'opacity-0'} absolute bottom-0 h-1/2 w-full bg-gradient-to-t from-black to-transparent duration-500 ease-in-out`}>
+                            <h1 className={`relative text-center text-2xl text-white/80 ${isOpen == idx ? 'top-[150px] opacity-100' : 'top-[200px] opacity-0'} duration-500 ease-in-out`}>
+                                {slide?.title}
+                            </h1>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </CodeBox>
+    );
+};
+
+export default Accordion12;
--- src/app/components/loading.js
+++ src/app/components/loading.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function loading() {
     return (
-        <div className="item-center container relative my-auto flex h-96 items-center justify-center">
+        <div className="relative flex h-96 w-full items-center justify-center">
             <span className="absolute h-12 w-12 animate-[u_1s_linear_infinite] rounded-full border-4 border-sky-400 border-l-transparent border-r-transparent"></span>
             <span className="absolute h-16 w-16 animate-[spin_1s_linear_infinite] rounded-full border-4 border-sky-500 border-l-transparent border-r-transparent"></span>
             <span className="absolute h-[45px] w-[45px]">
