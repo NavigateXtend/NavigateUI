1a58fdffc44bdd569b84adbffa0fb275ec44f2bc
Nasif Ahmed <nasif2ahmed@gamil.com>
2024-02-14 22:40:56 +0600

    _updated : fix for navbar2 and navbar6

--- src/Components/NavBar/NavBar2/NavBar2.jsx
+++ src/Components/NavBar/NavBar2/NavBar2.jsx
@@ -18,91 +18,25 @@ export const NavBar2 = () => {
 
     useEffect(() => {
         const closeDropDown = (e) => {
-            if (!dropDownMenuRef.current.contains(e.target)) {
+            if (!dropDownMenuRef?.current?.contains(e?.target)) {
                 setDropDownState(false);
             }
         };
 
         document.addEventListener('mousedown', closeDropDown);
+
+        return () => {
+            document.removeEventListener('mousedown', closeDropDown);
+        };
     }, []);
 
     return (
-        <nav className="flex items-center justify-start gap-20 px-10 py-8">
-            <img src="/Logo.svg" alt="navigateui logo" />
-            <ul className="flex items-center justify-between gap-10">
-                <li className="cursor-pointer hover:text-sky-500">Home</li>
-                <li>
-                    <button ref={dropDownMenuRef} onClick={() => setDropDownState(!dropDownState)} className="relative flex cursor-pointer  hover:text-sky-500">
-                        <span>Services</span>
-                        {dropDownState ? (
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="20"
-                                height="20"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                            >
-                                <path d="m18 15-6-6-6 6" />
-                            </svg>
-                        ) : (
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="20"
-                                height="20"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                            >
-                                <path d="m6 9 6 6 6-6" />
-                            </svg>
-                        )}
-                    </button>
-                    {dropDownState && (
-                        <ul className="absolute z-10 flex flex-col gap-2 bg-white py-2">
-                            <li className="cursor-pointer hover:text-sky-500">Food</li>
-                            <li className="cursor-pointer hover:text-sky-500">Transport</li>
-                            <li className="cursor-pointer hover:text-sky-500">Housing</li>
-                            <li className="cursor-pointer hover:text-sky-500">Management</li>
-                        </ul>
-                    )}
-                </li>
-                <li className="cursor-pointer hover:text-sky-500">About</li>
-                <li className="cursor-pointer hover:text-sky-500">Contact</li>
-            </ul>
-        </nav>
-    );
-};
-
-   `;
-
-    const [dropDownState, setDropDownState] = useState(false);
-    const dropDownMenuRef = useRef();
-
-    useEffect(() => {
-        // const closeDropDown = (e) => {
-        //     if (!dropDownMenuRef.current.contains(e.target)) {
-        //         setDropDownState(false);
-        //     }
-        // };
-
-        document.addEventListener('mousedown', closeDropDown);
-    }, []);
-
-    return (
-        <CodeBox codeStr={codeStr}>
             <nav className="flex items-center justify-start gap-20 bg-sky-100 px-10 py-8">
-                <Image src="/Logo.svg" alt="navigateui logo" width={45} height={45} />
+                <img src="/Logo.svg" alt="navigateui logo" />
                 <ul className="flex items-center justify-between gap-10">
                     <li className="cursor-pointer hover:text-sky-500">Home</li>
-                    <li>
-                        <button ref={dropDownMenuRef} onClick={() => setDropDownState(!dropDownState)} className="relative flex cursor-pointer  hover:text-sky-500">
+                    <li className="relative transition-transform">
+                        <div ref={dropDownMenuRef} onClick={() => setDropDownState(!dropDownState)} className="flex cursor-pointer hover:text-sky-500">
                             <span>Services</span>
                             {dropDownState ? (
                                 <svg
@@ -133,9 +67,83 @@ export const NavBar2 = () => {
                                     <path d="m6 9 6 6 6-6" />
                                 </svg>
                             )}
-                        </button>
+                        </div>
                         {dropDownState && (
-                            <ul className="absolute z-10 flex flex-col gap-2 bg-white px-5 py-2">
+                            <ul className="absolute z-10 mt-1 flex flex-col gap-2 bg-white px-5 py-2 shadow-sm ">
+                                <li className="cursor-pointer hover:text-sky-500">Food</li>
+                                <li className="cursor-pointer hover:text-sky-500">Transport</li>
+                                <li className="cursor-pointer hover:text-sky-500">Housing</li>
+                                <li className="cursor-pointer hover:text-sky-500">Management</li>
+                            </ul>
+                        )}
+                    </li>
+                    <li className="cursor-pointer hover:text-sky-500">About</li>
+                    <li className="cursor-pointer hover:text-sky-500">Contact</li>
+                </ul>
+            </nav>
+    );
+};
+
+   `;
+
+    const [dropDownState, setDropDownState] = useState(false);
+    const dropDownMenuRef = useRef();
+
+    useEffect(() => {
+        const closeDropDown = (e) => {
+            if (!dropDownMenuRef?.current?.contains(e?.target)) {
+                setDropDownState(false);
+            }
+        };
+
+        document.addEventListener('mousedown', closeDropDown);
+
+        return () => {
+            document.removeEventListener('mousedown', closeDropDown);
+        };
+    }, []);
+
+    return (
+        <CodeBox codeStr={codeStr}>
+            <nav className="mb-24 flex items-center justify-start gap-20 bg-sky-100 px-10 py-8">
+                <Image src="/Logo.svg" alt="navigateui logo" width={45} height={45} />
+                <ul className="flex items-center justify-between gap-10">
+                    <li className="cursor-pointer hover:text-sky-500">Home</li>
+                    <li className="relative transition-transform">
+                        <div ref={dropDownMenuRef} onClick={() => setDropDownState(!dropDownState)} className="flex cursor-pointer hover:text-sky-500">
+                            <span>Services</span>
+                            {dropDownState ? (
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="20"
+                                    height="20"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                >
+                                    <path d="m18 15-6-6-6 6" />
+                                </svg>
+                            ) : (
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="20"
+                                    height="20"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                >
+                                    <path d="m6 9 6 6 6-6" />
+                                </svg>
+                            )}
+                        </div>
+                        {dropDownState && (
+                            <ul className="absolute z-10 mt-1 flex flex-col gap-2 bg-white px-5 py-2 shadow-sm ">
                                 <li className="cursor-pointer hover:text-sky-500">Food</li>
                                 <li className="cursor-pointer hover:text-sky-500">Transport</li>
                                 <li className="cursor-pointer hover:text-sky-500">Housing</li>
