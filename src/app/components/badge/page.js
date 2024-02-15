a8fc56e774e58ecb86f66dae944efec4602fe907
Mohammad Arif <programmermdarif@gmail.com>
2024-02-15 11:20:18 +0600

    _added : Badge 3

--- src/app/components/badge/page.js
+++ src/app/components/badge/page.js
@@ -1,17 +1,17 @@
-import React from 'react';
 import Badge1 from '@/Components/Badge/Badge1/Badge1';
 import Badge2 from '@/Components/Badge/Badge2/Badge2';
+import Badge3 from '@/Components/Badge/Badge3/Badge3';
 const page = () => {
     return (
         <div>
-        
-           <h1 className="text-2xl font-semibold px-4 mt-10 text-white"># Simple notification badge</h1>
+            <h1 className="mt-10 px-4 text-lg   font-semibold text-white md:text-xl lg:text-2xl"># Simple notification badge</h1>
             <Badge1></Badge1>
-            <h1 className="text-2xl font-semibold px-4 mt-10 text-white"># Notification badge with numbers</h1>
+            <h1 className="mt-10 px-4 text-lg   font-semibold text-white md:text-xl lg:text-2xl"># Notification badge with numbers</h1>
             <Badge2></Badge2>
+            <h1 className="mt-10 px-4 text-lg   font-semibold text-white md:text-xl lg:text-2xl"># Home badge with below numbers</h1>
+            <Badge3></Badge3>
         </div>
-
-    )
+    );
 };
 
 export default page;
