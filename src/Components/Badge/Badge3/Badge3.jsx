efde90f6765897a63bd8893211708c35008dcbd2
Mohammad Arif <programmermdarif@gmail.com>
2024-02-15 11:39:26 +0600

    _added : Badge 3

--- src/Components/Badge/Badge3/Badge3.jsx
+++ src/Components/Badge/Badge3/Badge3.jsx
@@ -4,10 +4,10 @@ import React from 'react';
 const codeStr = `
 export const Badge3 = () => {
     return (
-      <div className="relative mx-auto bg-white hover:bg-gray-200 p-2 rounded-md w-fit h-fit">
-          <svg width="40px"  viewBox="-4.32 -4.32 32.64 32.64" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#0284C7"><g id="SVGRepo_bgCarrier" strokeWidth={0} /><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" stroke="#CCCCCC" strokeWidth="0.048" /><g id="SVGRepo_iconCarrier"> <path d="M8.35179 20.2418C9.19288 21.311 10.5142 22 12 22C13.4858 22 14.8071 21.311 15.6482 20.2418C13.2264 20.57 10.7736 20.57 8.35179 20.2418Z" fill="#38BDF8" /> <path d="M18.7491 9V9.7041C18.7491 10.5491 18.9903 11.3752 19.4422 12.0782L20.5496 13.8012C21.5612 15.3749 20.789 17.5139 19.0296 18.0116C14.4273 19.3134 9.57274 19.3134 4.97036 18.0116C3.21105 17.5139 2.43882 15.3749 3.45036 13.8012L4.5578 12.0782C5.00972 11.3752 5.25087 10.5491 5.25087 9.7041V9C5.25087 5.13401 8.27256 2 12 2C15.7274 2 18.7491 5.13401 18.7491 9Z" fill="#38BDF8" /> </g></svg>
-          <span className="absolute -right-2 -top-2 flex h-[24px] w-[24px] items-center justify-center rounded-full bg-red-500 text-center text-[12px] text-white ">12</span>
-      </div>
+    <div className="relative mx-auto bg-white hover:bg-gray-200 p-2 rounded-md w-fit h-fit">
+        <svg width="40px" fill="#0284C7" viewBox="-1.92 -1.92 27.84 27.84" id="home-alt-2" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" className="icon flat-color" stroke="#0284C7"><g id="SVGRepo_bgCarrier" strokeWidth={0} /><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" stroke="#CCCCCC" strokeWidth="0.048" /><g id="SVGRepo_iconCarrier"><path id="primary" d="M20,10V21a1,1,0,0,1-1,1H15.5a1,1,0,0,1-1-1V16.1a1,1,0,0,0-1-1h-3a1,1,0,0,0-1,1V21a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V10a1,1,0,0,1,.29-.71l7-7a1,1,0,0,1,1.42,0l7,7A1,1,0,0,1,20,10Z" style={{fill: '#38BDF8'}} /></g></svg>
+        <span className="absolute -right-2 -bottom-2 flex h-[24px] w-[24px] items-center justify-center rounded-full bg-red-500 text-center text-[12px] text-white ">12</span>
+    </div>
     );
 };
 `
--- src/app/components/badge/page.js
+++ src/app/components/badge/page.js
@@ -1,6 +1,7 @@
 import Badge1 from '@/Components/Badge/Badge1/Badge1';
 import Badge2 from '@/Components/Badge/Badge2/Badge2';
 import Badge3 from '@/Components/Badge/Badge3/Badge3';
+import Badge4 from '@/Components/Badge/Badge4/Badge4';
 const page = () => {
     return (
         <div>
@@ -10,6 +11,8 @@ const page = () => {
             <Badge2></Badge2>
             <h1 className="mt-10 px-4 text-lg   font-semibold text-white md:text-xl lg:text-2xl"># Home badge with below numbers</h1>
             <Badge3></Badge3>
+            <h1 className="mt-10 px-4 text-lg   font-semibold text-white md:text-xl lg:text-2xl"># Badge with text </h1>
+            <Badge4></Badge4>
         </div>
     );
 };
