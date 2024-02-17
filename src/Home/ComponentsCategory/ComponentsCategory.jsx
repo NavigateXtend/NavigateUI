a27d4abca43752219463536b63bf8955bfd09933
Mohammad Arif <programmermdarif@gmail.com>
2024-02-17 16:08:47 +0600

    _added : a new seciton with 8 cards

--- src/Home/ComponentsCategory/ComponentsCategory.jsx
+++ src/Home/ComponentsCategory/ComponentsCategory.jsx
@@ -10,32 +10,32 @@ const ComponentsCategory = () => {
         },
 
         {
-            name: 'Accordin',
-            Image: 'https://i.ibb.co/swV2hBn/Screenshot-1499.png'
+            name: 'Avatar',
+            Image: 'https://i.ibb.co/d081N9w/Screenshot-1503-removebg-preview.png'
         },
         {
-            name: 'Accordin',
-            Image: 'https://i.ibb.co/swV2hBn/Screenshot-1499.png'
+            name: 'Carousel',
+            Image: 'https://i.ibb.co/RTy7nKF/Screenshot-1504-removebg-preview.png'
         },
         {
-            name: 'Accordin',
-            Image: 'https://i.ibb.co/swV2hBn/Screenshot-1499.png'
+            name: 'Card',
+            Image: 'https://i.ibb.co/Gdv28B7/Screenshot-1505-removebg-preview.png'
         },
         {
-            name: 'Accordin',
-            Image: 'https://i.ibb.co/swV2hBn/Screenshot-1499.png'
+            name: 'Form',
+            Image: 'https://i.ibb.co/Pmbj9Nm/Screenshot-1506-removebg-preview.png'
         },
         {
-            name: 'Accordin',
-            Image: 'https://i.ibb.co/swV2hBn/Screenshot-1499.png'
+            name: 'Footer',
+            Image: 'https://i.ibb.co/2MjvQhY/Screenshot-1507-removebg-preview.png'
         },
         {
-            name: 'Accordin',
-            Image: 'https://i.ibb.co/swV2hBn/Screenshot-1499.png'
+            name: 'Hero',
+            Image: 'https://i.ibb.co/tm0g3Ms/Screenshot-1508.png'
         },
         {
-            name: 'Accordin',
-            Image: 'https://i.ibb.co/swV2hBn/Screenshot-1499.png'
+            name: 'Modal',
+            Image: 'https://i.ibb.co/VqKH9V5/Screenshot-1511.png'
         }
     ];
 
@@ -51,13 +51,13 @@ const ComponentsCategory = () => {
             </p>
             <div className="mt-10 grid grid-cols-1 gap-5 sm:mt-16 md:mt-24 lg:grid-cols-2 xl:grid-cols-4">
                 {Data.map((item) => (
-                    <div key={item} className="flex flex-col rounded-md p-2 transition-all duration-200 hover:scale-110 hover:bg-gray-800">
-                        <div className="mx-auto w-full rounded-md bg-gray-900 p-5 shadow-md ">
-                            <Image src={item.Image} alt="navigateui logo" width={300} height={45} className="mx-auto max-h-[140px] w-fit" />
+                    <div key={item} className="flex cursor-pointer max-w-[400px] mx-auto flex-col rounded-md p-2 transition-all duration-200 hover:scale-110 hover:bg-gray-800">
+                        <div className="mx-auto flex h-[230px] w-full items-center rounded-md bg-gray-900 p-5 py-5 shadow-md ">
+                            <Image src={item.Image} alt="navigateui logo" width={300} height={45} className="mx-auto max-h-[200px] w-fit" />
                         </div>
                         <div className="px-3">
-                            <h2 className="mt-3 text-start text-white">{item.name}</h2>
-                            <p className="text-start text-[12px] text-gray-400">Explore a wide range of unique {item.name.toLowerCase()} from our website</p>
+                            <h2 className="mt-3 text-start text-lg  text-white">{item.name}</h2>
+                            <p className="text-start text-[15px] text-gray-400">Explore a wide range of unique {item.name.toLowerCase()} from our website</p>
                         </div>
                     </div>
                 ))}
