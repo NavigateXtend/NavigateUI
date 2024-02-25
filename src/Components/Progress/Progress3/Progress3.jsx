789ea3f19a82babfa1ece2cc3a9717db3315570b
Mohammad Arif <programmermdarif@gmail.com>
2024-02-25 11:49:11 +0600

    Update progress barnch (#219)

--- src/Components/Progress/Progress3/Progress3.jsx
+++ src/Components/Progress/Progress3/Progress3.jsx
@@ -6,7 +6,7 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 
 const codestr =`
-export const Progress1 = () => {
+export const Progress3 = () => {
     const ProgressNumber = 76; // adjust the number to increase the progress number
     return (
     <div className="flex flex-col w-[300px] mx-auto gap-2">
@@ -20,19 +20,21 @@ export const Progress1 = () => {
 `
 
 
-const Progress1 = () => {
+const Progress3 = () => {
 
-    const ProgressNumber = 76; // adjust the number to increase the progress number
+    const ProgressNumber = 58; // adjust the number to increase the progress number
     return (
         <CodeBox codeStr={codestr}>
-            <div className="flex flex-col w-[300px] mx-auto gap-2">
-                <div className={`flex h-3 w-full  items-center justify-center rounded-full bg-sky-300`}>
-                    <div style={{ width: `${ProgressNumber}%` }} className={`transition-width mr-auto h-3 w-0 rounded-full  bg-sky-500 duration-500`} ></div>
+            <div className="flex flex-col w-[500px] mx-auto gap-2">
+                <div className={`flex h-7 w-full p-1 items-center justify-center rounded-full bg-sky-300`}>
+                    <div style={{ width: `${ProgressNumber}%` }} className={`transition-width flex justify-center items-center mr-auto h-5 w-0 rounded-full  bg-sky-600 duration-500`} >
+                    <span className="font-medium text-sm text-center text-white"> {ProgressNumber} %</span>
+                    </div>
                 </div>
-                <span className="text-lg font-medium text-center text-sky-500"> {ProgressNumber} %</span>
+                
             </div>
         </CodeBox>
     );
 };
 
-export default Progress1;
+export default Progress3;
--- src/app/components/progress/page.js
+++ src/app/components/progress/page.js
@@ -2,6 +2,8 @@
 
 
 import  Progress1  from '@/Components/Progress/Progress1/Progress1';
+import  Progress2  from '@/Components/Progress/Progress2/Progress2';
+import  Progress3  from '@/Components/Progress/Progress3/Progress3';
 
 export const metadata = {
     title: 'Progress Components',
@@ -13,7 +15,10 @@ const Progress = () => {
         <div className="flex flex-col items-center justify-center">
             <h1 className="mt-10 px-4 text-2xl font-semibold text-gray-100"># Basic dynamic progress</h1>
             <Progress1 />
-
+            <h1 className="mt-10 px-4 text-2xl font-semibold text-gray-100"># Basic dynamic animated progress</h1>
+            <Progress2 />
+            <h1 className="mt-10 px-4 text-2xl font-semibold text-gray-100"># Dynamic progress bar with inside text</h1>
+            <Progress3 />
         </div>
     );
 };
