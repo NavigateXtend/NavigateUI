77f1d225640ae9553f6950453c4ed5874319294d
Mohammad Arif <programmermdarif@gmail.com>
2024-02-25 15:02:30 +0600

    _added : Dynamic animated progress bar with outside text

--- src/Components/Progress/Progress2/Progress2.jsx
+++ src/Components/Progress/Progress2/Progress2.jsx
@@ -17,7 +17,7 @@ export const Progress2 = () => {
     return (
             <div className="flex flex-col w-[300px] gap-2">
                 <div className={\`flex h-3 w-full  items-center justify-center rounded-full bg-sky-300\`}>
-                    <div style={{ width: \`\${loaded ? ProgressNumber : '0'}%\` }} className={\`transition-width mr-auto h-3 w-0 rounded-full  bg-sky-500 duration-500\`} ></div>
+                    <div style={{ width: \`\${loaded ? ProgressNumber : '0'}%\` }} className={\`transition-width mr-auto h-3 w-0 rounded-full  bg-sky-600 duration-500\`} ></div>
                 </div>
                 <span className="text-lg font-medium text-center text-sky-400"> {ProgressNumber} %</span>
             </div>
@@ -38,7 +38,7 @@ const Progress2 = () => {
         <CodeBox codeStr={codestr}>
             <div className="flex flex-col w-[300px] mx-auto gap-2">
                 <div className={`flex h-3 w-full  items-center justify-center rounded-full bg-sky-300`}>
-                    <div style={{ width: `${loaded ? ProgressNumber : '0'}%` }} className={`transition-width mr-auto h-3 w-0 rounded-full  bg-sky-500 duration-500`} ></div>
+                    <div style={{ width: `${loaded ? ProgressNumber : '0'}%` }} className={`transition-width mr-auto h-3 w-0 rounded-full  bg-sky-600 duration-500`} ></div>
                 </div>
                 <span className="text-lg font-medium text-center text-sky-500"> {ProgressNumber} %</span>
             </div>
--- src/Components/Progress/Progress3/Progress3.jsx
+++ src/Components/Progress/Progress3/Progress3.jsx
@@ -9,11 +9,12 @@ const codestr =`
 export const Progress3 = () => {
     const ProgressNumber = 76; // adjust the number to increase the progress number
     return (
-    <div className="flex flex-col w-[300px] mx-auto gap-2">
-        <div className={\`flex h-3 w-full  items-center justify-center rounded-full bg-sky-300\`}>
-            <div style={{ width: \`\${ProgressNumber}%\` }} className={\`transition-width mr-auto h-3 w-0 rounded-full  bg-sky-500 duration-500\`} ></div>
+        <div className="flex flex-col w-[500px] mx-auto gap-2">
+        <div className={\`flex h-7 w-full  items-center justify-center rounded-full bg-sky-300\`}>
+            <div style={{ width: \`\${ProgressNumber}%\` }} className={\`transition-width flex justify-center items-center mr-auto h-full w-0 rounded-full  bg-sky-600 duration-500\`} >
+            <span className="font-medium  text-center text-white"> {ProgressNumber} %</span>
+            </div>
         </div>
-        <span className="text-lg font-medium text-center text-sky-500"> {ProgressNumber} %</span>
     </div>
     );
 };
@@ -22,16 +23,15 @@ export const Progress3 = () => {
 
 const Progress3 = () => {
 
-    const ProgressNumber = 58; // adjust the number to increase the progress number
+    const ProgressNumber = 41; // adjust the number to increase the progress number
     return (
         <CodeBox codeStr={codestr}>
             <div className="flex flex-col w-[500px] mx-auto gap-2">
-                <div className={`flex h-7 w-full p-1 items-center justify-center rounded-full bg-sky-300`}>
-                    <div style={{ width: `${ProgressNumber}%` }} className={`transition-width flex justify-center items-center mr-auto h-5 w-0 rounded-full  bg-sky-600 duration-500`} >
-                    <span className="font-medium text-sm text-center text-white"> {ProgressNumber} %</span>
+                <div className={`flex h-5 w-full  items-center justify-center rounded-full bg-sky-300`}>
+                    <div style={{ width: `${ProgressNumber}%` }} className={`transition-width flex justify-center items-center mr-auto h-full w-0 rounded-full  bg-sky-600 duration-500`} >
+                    <span className="font-medium text-sm text-center text-gray-300"> {ProgressNumber} %</span>
                     </div>
                 </div>
-                
             </div>
         </CodeBox>
     );
