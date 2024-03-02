92392490f62122399ada6a0a099a3151beb4d5ee
Mohammad Arif <programmermdarif@gmail.com>
2024-03-02 20:00:44 +0600

    _update:# Basic dynamic animated progress

--- src/Components/Progress/Progress2/Progress2.jsx
+++ src/Components/Progress/Progress2/Progress2.jsx
@@ -7,17 +7,18 @@ import { useEffect } from 'react';
 
 const codestr = `
 export const Progress2 = () => {
-    const [loaded, setLoaded] = useState(false);
-    useEffect(() => {
-        setTimeout(() => {
-            setLoaded(true);
-        }, 500); // Set the delay to adjust the animation
-    }, []);
-    const ProgressNumber = 56; // adjust the number to increase the progress number
+  const [progressNumber, setProgressNumber] = useState(0);
+  const ProgressNumberPercent = 90; // Adjust the number for your project.
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgressNumber((prevNumber) => (prevNumber === ProgressNumberPercent ? 0 : prevNumber + 1));
+    }, 30); // Adjust the interval as per your requirement
+    return () => clearInterval(interval); // Cleanup function to clear the interval
+  }, []);
     return (
             <div className="flex flex-col w-[300px] gap-2">
                 <div className={\`flex h-3 w-full  items-center justify-center rounded-full bg-sky-300\`}>
-                    <div style={{ width: \`\${loaded ? ProgressNumber : '0'}%\` }} className={\`transition-width mr-auto h-3 w-0 rounded-full  bg-sky-600 duration-500\`} ></div>
+                    <div style={{ width: \`\${progressNumber}%\` }} className={\`transition-width mr-auto h-3 w-0 rounded-full  bg-sky-600 duration-500\`} ></div>
                 </div>
                 <span className="text-lg font-medium text-center text-sky-400"> {ProgressNumber} %</span>
             </div>
@@ -26,20 +27,21 @@ export const Progress2 = () => {
 `;
 
 const Progress2 = () => {
-  const [loaded, setLoaded] = useState(false);
+  const [progressNumber, setProgressNumber] = useState(0);
+  const ProgressNumberPercent = 90; // Adjust the number for your project.
   useEffect(() => {
-    setTimeout(() => {
-      setLoaded(true);
-    }, 500); // Set the delay to adjust the animation
+    const interval = setInterval(() => {
+      setProgressNumber((prevNumber) => (prevNumber === ProgressNumberPercent ? 0 : prevNumber + 1));
+    }, 30); // Adjust the interval as per your requirement
+    return () => clearInterval(interval); // Cleanup function to clear the interval
   }, []);
-  const ProgressNumber = 56; // adjust the number to increase the progress number
   return (
     <CodeBox codeStr={codestr}>
       <div className="mx-auto flex w-[300px] flex-col gap-2">
-        <div className={`flex h-3 w-full  items-center justify-center rounded-full bg-sky-300`}>
-          <div style={{ width: `${loaded ? ProgressNumber : '0'}%` }} className={`transition-width mr-auto h-3 w-0 rounded-full  bg-sky-600 duration-500`}></div>
+        <div className={`flex h-3 w-full items-center justify-center rounded-full bg-sky-300`}>
+          <div style={{ width: `${progressNumber}%` }} className={`transition-width mr-auto h-3 rounded-full bg-sky-600 duration-[1ms]`}></div>
         </div>
-        <span className="text-center text-lg font-medium text-sky-500"> {ProgressNumber} %</span>
+        <span className="text-center text-lg font-medium text-sky-500">{progressNumber} %</span>
       </div>
     </CodeBox>
   );
