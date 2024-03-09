0f50366782131526e3e2220c66b731ab7190e9dd
Mohammad Arif <programmermdarif@gmail.com>
2024-03-09 22:35:02 +0600

    _added : SquareCardPagination and also TransParentPagination with codestr

--- src/app/components/pagination/page.js
+++ src/app/components/pagination/page.js
@@ -1,21 +1,22 @@
 import SimplePagination from '@/Components/Pagination/SimplePagination';
 import CardPagination from '@/Components/Pagination/CardPagination';
+import SqureCardPagination from '@/Components/Pagination/SqureCardPagination';
+import TransParentPagination from '@/Components/Pagination/TransParentPagination';
+
 import React from 'react';
 
-
-
-
 export const metadata = {
-    title: 'Pagination Components',
-    description: 'Pagination tailwind components'
-  }; 
-
-export default function page () {
-    return (
-        <div className="flex flex-col items-center justify-center">
-        <SimplePagination />
-        <CardPagination></CardPagination>
-        </div>
-    );
+  title: 'Pagination Components',
+  description: 'Pagination tailwind components'
 };
 
+export default function page() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <SimplePagination />
+      <CardPagination></CardPagination>
+      <SqureCardPagination></SqureCardPagination>
+      <TransParentPagination></TransParentPagination>
+    </div>
+  );
+}
