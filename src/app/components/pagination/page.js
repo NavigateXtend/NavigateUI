82be38165b933199109c499f3d8ee91b72659c35
Mohammad Arif <programmermdarif@gmail.com>
2024-03-09 21:55:34 +0600

    _updated : simple paginaiton and also added : cardType Pagination

--- src/app/components/pagination/page.js
+++ src/app/components/pagination/page.js
@@ -1,19 +1,20 @@
 import SimplePagination from '@/Components/Pagination/SimplePagination';
+import CardPagination from '@/Components/Pagination/CardPagination';
 import React from 'react';
 
 
 
 
 export const metadata = {
-    title: 'Modal Components',
-    description: 'Modal tailwind components'
+    title: 'Pagination Components',
+    description: 'Pagination tailwind components'
   }; 
 
 export default function page () {
     return (
         <div className="flex flex-col items-center justify-center">
-        <h1 className="mt-10 px-4 text-lg  font-semibold text-white md:text-xl lg:text-2xl"># Simple progress</h1>
         <SimplePagination />
+        <CardPagination></CardPagination>
         </div>
     );
 };
