82fab1747c055d9b9b46765c9327b0e55c8ee310
Mohammad Arif <programmermdarif@gmail.com>
2024-03-10 08:07:12 +0600

    Pagination (#230)

--- src/app/components/pagination/page.js
+++ src/app/components/pagination/page.js
@@ -2,6 +2,7 @@ import SimplePagination from '@/Components/Pagination/SimplePagination';
 import CardPagination from '@/Components/Pagination/CardPagination';
 import SqureCardPagination from '@/Components/Pagination/SqureCardPagination';
 import TransParentPagination from '@/Components/Pagination/TransParentPagination';
+import SquareCardPaginaiton2 from '@/Components/Pagination/SquareCardPaginaiton2';
 
 import React from 'react';
 
@@ -16,7 +17,10 @@ export default function page() {
       <SimplePagination />
       <CardPagination></CardPagination>
       <SqureCardPagination></SqureCardPagination>
+      <SquareCardPaginaiton2></SquareCardPaginaiton2>
       <TransParentPagination></TransParentPagination>
+
+
     </div>
   );
 }
