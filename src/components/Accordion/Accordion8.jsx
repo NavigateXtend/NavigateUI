8071c729de01b29891423d93d05f66caccf5ba02
Shiyam Sarker <78273465+shiyam-sarker10@users.noreply.github.com>
2024-03-29 23:30:01 +0600

    fixed responsive issue of Accordions

--- src/components/Accordion/Accordion8.jsx
+++ src/components/Accordion/Accordion8.jsx
@@ -12,23 +12,25 @@ const accordionData = [{title: 'heading', subtitle: 'Hi!~ How are you doing?', c
         setIsActive((prevIdx) => (prevIdx === idx ? null : idx));
     };
     return (
-           <div className="w-fit mx-auto min-h-[300px] flex shadow-md overflow-hidden">
-                {accordionData.map((_, idx) => (
-                    <div key={idx} className="flex">
-                        {/* toggle item */}
-                        <button onClick={() => handleToggle(idx)} className={\`h-full w-fit flex items-end pb-10 bg-white border-b-8 \${_.colorBorder} shadow-[2px_2px_5px_#00000083]\`}>
-                            <div className={\`w-16 h-16 text-white \${_.colorBg} relative flex items-center justify-center\`}>
-                                <span className={\`w-0 h-0 \${_.colorBorder} border-r-[60px] border-b-[60px] rounded-lg border-r-transparent absolute rotate-[225deg] left-8 -z-50\`}></span>0{idx + 1}
-                            </div>
-                        </button>
-                        {/* container */}
-                        <div className={\`grid place-content-center  \${isActive === idx ? 'w-56 px-5 opacity-1 scale-1' : 'w-0 opacity-0 scale-0'} text-black duration-300 ease-in-out\`}>
-                        <h2 className="font-black lg:text-2xl text-white">{_.title}</h2>
-                        <p className="text-gray-200">{_.subtitle}</p>
+        <div className="mx-auto my-10 flex w-fit rotate-90 shadow-md min-h-[220px] md:min-h-[300px] md:rotate-0 ">
+            {accordionData?.map((data, idx) => (
+                <div key={idx} className="flex">
+                    {/* toggle item */}
+                    <button onClick={() => handleToggle(idx)} className={\` flex h-full w-fit items-end border-b-8 bg-white pb-10 dark:bg-[#323238] \${data?.colorBorder} shadow-[2px_2px_5px_#00000083]\`}>
+                        <div className={\`size-10 text-white md:size-16 \${data?.colorBg} relative flex items-center justify-center \`}>
+                            <span className={\`h-0 w-0 \${data?.colorBorder} dark:\${data?.colorBorder} absolute left-8 -z-10 hidden  rotate-[225deg] rounded-lg border-b-[60px] border-r-[60px] border-r-transparent md:block\`}></span>
+                            <span className="-rotate-90 md:rotate-0">0{idx + 1}</span>
                         </div>
+                    </button>
+                    {/* container */}
+                    <div
+                        className={\`grid place-content-center \${isActive === idx ? 'opacity-1 scale-1  w-[220px] md:w-56   px-5' : 'w-0 scale-0 opacity-0'} -z-20  -rotate-90 bg-white  text-black duration-300 ease-in-out dark:bg-[#18181B] md:rotate-0\`}>
+                        <h2 className="font-black dark:text-white lg:text-2xl ">{data?.title}</h2>
+                        <p className="dark:text-white/60 ">{data?.subtitle}</p>
                     </div>
-                ))}
-            </div>
+                </div>
+            ))}
+        </div>
     );
 };
 
@@ -74,24 +76,34 @@ export default function Accordion8() {
     setIsActive((prevIdx) => (prevIdx === idx ? null : idx));
   };
   return (
-    <CP code={code}>
-      <div className="mx-auto flex min-h-[300px] w-fit overflow-hidden shadow-md">
-        {accordionData.map((_, idx) => (
-          <div key={idx} className="flex">
-            {/* toggle item */}
-            <button onClick={() => handleToggle(idx)} className={`flex h-full w-fit items-end border-b-8 bg-white pb-10 ${_.colorBorder} shadow-[2px_2px_5px_#00000083]`}>
-              <div className={`h-16 w-16 text-white ${_.colorBg} relative flex items-center justify-center`}>
-                <span className={`h-0 w-0 ${_.colorBorder} absolute left-8 -z-50 rotate-[225deg] rounded-lg border-b-[60px] border-r-[60px] border-r-transparent`}></span>0{idx + 1}
+      <CP code={code}>
+          <div className="flex h-screen justify-center items-center md:block  md:h-auto">
+              <div className="mx-auto my-10 flex w-fit rotate-90 shadow-md min-h-[220px] md:min-h-[300px] md:rotate-0 ">
+                  {accordionData?.map((data, idx) => (
+                      <div key={idx} className="flex">
+                          {/* toggle item */}
+                          <button
+                              onClick={() => handleToggle(idx)}
+                              className={` flex h-full w-fit items-end border-b-8 bg-white pb-10 dark:bg-[#323238] ${data?.colorBorder} shadow-[2px_2px_5px_#00000083]`}
+                          >
+                              <div className={`size-10 text-white md:size-16 ${data?.colorBg} relative flex items-center justify-center `}>
+                                  <span
+                                      className={`h-0 w-0 ${data?.colorBorder} dark:${data?.colorBorder} absolute left-8 -z-10 hidden  rotate-[225deg] rounded-lg border-b-[60px] border-r-[60px] border-r-transparent md:block`}
+                                  ></span>
+                                  <span className="-rotate-90 md:rotate-0">0{idx + 1}</span>
+                              </div>
+                          </button>
+                          {/* container */}
+                          <div
+                              className={`grid place-content-center  ${isActive === idx ? 'opacity-1 scale-1  w-[220px] md:w-56   px-5' : 'w-0 scale-0 opacity-0'} -z-20  -rotate-90 bg-white  text-black duration-300 ease-in-out dark:bg-[#18181B] md:rotate-0`}
+                          >
+                              <h2 className="font-black dark:text-white lg:text-2xl ">{data?.title}</h2>
+                              <p className="dark:text-white/60 ">{data?.subtitle}</p>
+                          </div>
+                      </div>
+                  ))}
               </div>
-            </button>
-            {/* container */}
-            <div className={`grid place-content-center  ${isActive === idx ? 'opacity-1 scale-1 w-56 px-5' : 'w-0 scale-0 opacity-0'} text-black duration-300 ease-in-out`}>
-              <h2 className="font-black text-white lg:text-2xl">{_.title}</h2>
-              <p className="text-gray-200">{_.subtitle}</p>
-            </div>
           </div>
-        ))}
-      </div>
-    </CP>
+      </CP>
   );
 }
--- src/components/Accordion/Accordion9.jsx
+++ src/components/Accordion/Accordion9.jsx
@@ -12,27 +12,22 @@ const accordionData = [{title: 'heading', subtitle: 'Hi!~ How are you doing?', c
         setIsActive((prevIdx) => (prevIdx === idx ? null : idx));
     };
     return (
-             <div className="w-fit mx-auto min-h-[300px] flex gap-1">
-                {accordionData.map((_, idx) => (
-                    <div key={idx} className="flex">
-                        {/* toggle item */}
-                        <button onClick={() => handleToggle(idx)} className={\`h-full flex justify-around flex-col items-center w-14 \${_.colorBg} text-white rounded-lg relative\`}>
-                            {isActive === idx && <span className={\`w-0 h-0 \${_.colorBorder} border-r-[20px] border-b-[20px] border-r-transparent absolute rotate-[225deg] left-10 top-4\`}></span>}
-                            <p>{idx + 1}</p>
-                            <p className="rotate-[270deg]">Example</p>
-                        </button>
-                        {/* container */}
-                        <div
-                            className={\`grid place-content-center bg-gray-200 shadow-md rounded-lg \${
-                                isActive === idx ? 'w-56 px-5 opacity-1 scale-1' : 'w-0 opacity-0 scale-0'
-                            } text-black ml-2 duration-300 ease-in-out\`}
-                        >
-                            <h2 className="lg:text-2xl font-black">{_.title}</h2>
-                            <p className="text-black/60">{_.subtitle}</p>
-                        </div>
-                    </div>
-                ))}
-            </div>
+      <div className="mx-auto flex min-h-[300px] w-fit gap-1 rotate-90 md:rotate-0">
+          {accordionData?.map((data, idx) => (
+              <div key={idx} className="flex ">
+                  {/* toggle item */}
+                  <button onClick={() => handleToggle(idx)} className={\`flex h-full w-14 flex-col items-center justify-around \${data?.colorBg} relative rounded-lg text-white\`}> {isActive === idx && <span className={\`h-0 w-0 \${data?.colorBorder} absolute left-10 top-4 rotate-[225deg] border-b-[20px] border-r-[20px] border-r-transparent\`}></span>}
+                      <p className="-rotate-90 md:rotate-0">{idx + 1}</p>
+                      <p className="rotate-[270deg]">Example</p>
+                  </button>
+                  {/* container */}
+                  <div className={\`grid place-content-center rounded-lg bg-gray-200 dark:bg-[#18181B] shadow-md \${isActive === idx ? 'opacity-1 scale-1 h-[200px] md:h-auto w-[300px] md:w-56 px-5' : 'w-0 scale-0 opacity-0'} md:ml-2 mt-12 md:mt-0 text-black duration-300 ease-in-out -rotate-90 md:rotate-0\`}>
+                      <h2 className="font-black lg:text-2xl dark:text-white">{data?.title}</h2>
+                      <p className="text-black/60 dark:text-white/60">{data?.subtitle}</p>
+                  </div>
+              </div>
+          ))}
+      </div>
     );
 };
 
@@ -78,28 +73,30 @@ export default function Accordion9() {
     setIsActive((prevIdx) => (prevIdx === idx ? null : idx));
   };
   return (
-    <CP code={code}>
-      <div className="mx-auto flex min-h-[300px] w-fit gap-1">
-        {accordionData.map((_, idx) => (
-          <div key={idx} className="flex">
-            {/* toggle item */}
-            <button onClick={() => handleToggle(idx)} className={`flex h-full w-14 flex-col items-center justify-around ${_.colorBg} relative rounded-lg text-white`}>
-              {isActive === idx && <span className={`h-0 w-0 ${_.colorBorder} absolute left-10 top-4 rotate-[225deg] border-b-[20px] border-r-[20px] border-r-transparent`}></span>}
-              <p>{idx + 1}</p>
-              <p className="rotate-[270deg]">Example</p>
-            </button>
-            {/* container */}
-            <div
-              className={`grid place-content-center rounded-lg bg-gray-200 shadow-md ${
-                isActive === idx ? 'opacity-1 scale-1 w-56 px-5' : 'w-0 scale-0 opacity-0'
-              } ml-2 text-black duration-300 ease-in-out`}
-            >
-              <h2 className="font-black lg:text-2xl">{_.title}</h2>
-              <p className="text-black/60">{_.subtitle}</p>
-            </div>
+      <CP code={code}>
+          <div className='flex justify-center items-center h-screen md:h-auto md:block'>
+              <div className="mx-auto flex min-h-[300px] w-fit gap-1 rotate-90 md:rotate-0">
+                  {accordionData?.map((data, idx) => (
+                      <div key={idx} className="flex ">
+                          {/* toggle item */}
+                          <button onClick={() => handleToggle(idx)} className={`flex h-full w-14 flex-col items-center justify-around ${data?.colorBg} relative rounded-lg text-white`}>
+                              {isActive === idx && <span className={`h-0 w-0 ${data?.colorBorder} absolute left-10 top-4 rotate-[225deg] border-b-[20px] border-r-[20px] border-r-transparent`}></span>}
+                              <p className="-rotate-90 md:rotate-0">{idx + 1}</p>
+                              <p className="rotate-[270deg]">Example</p>
+                          </button>
+                          {/* container */}
+                          <div
+                              className={`grid place-content-center rounded-lg bg-gray-200 dark:bg-[#18181B] shadow-md ${
+                                  isActive === idx ? 'opacity-1 scale-1 h-[200px] md:h-auto w-[300px] md:w-56 px-5' : 'w-0 scale-0 opacity-0'
+                              } md:ml-2 mt-12 md:mt-0 text-black duration-300 ease-in-out -rotate-90 md:rotate-0`}
+                          >
+                              <h2 className="font-black lg:text-2xl dark:text-white">{data?.title}</h2>
+                              <p className="text-black/60 dark:text-white/60">{data?.subtitle}</p>
+                          </div>
+                      </div>
+                  ))}
+              </div>
           </div>
-        ))}
-      </div>
-    </CP>
+      </CP>
   );
 }
