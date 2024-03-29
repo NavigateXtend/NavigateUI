edd112906de921feb0357080197291dcefa29df7
Mahmud Hassan Lehri <lehrimirza101@gmail.com>
2024-03-29 16:38:48 +0600

    navbar 1 & 2 modified (#233)

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -1,3 +1,7 @@
 export default function Heading({ children }) {
-  return <h1 className="mt-24 text-lg text-black first-of-type:mt-10 sm:text-xl md:mt-32 lg:mt-44 dark:text-white">{children}</h1>;
+  return (
+    <h1 className="mt-24 text-lg text-black first-of-type:mt-10 dark:text-white sm:text-xl md:mt-32 lg:mt-44 lg:text-2xl">
+      #<span className="underline">{children}</span>
+    </h1>
+  );
 }
